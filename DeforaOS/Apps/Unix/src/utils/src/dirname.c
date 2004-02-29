cd2a87bb92dcd9ef741966b182bf295f7d7c6e1d
khorben <>
2004-02-29 04:35:58 +0000

    Better usage function

--- DeforaOS/Apps/Unix/src/utils/src/dirname.c
+++ DeforaOS/Apps/Unix/src/utils/src/dirname.c
@@ -6,15 +6,20 @@
 #include <stdio.h>
 
 
+/* usage */
+static int usage(void)
+{
+	fprintf(stderr, "%s", "Usage: dirname string\n");
+	return 1;
+}
+
+
 /* main */
 int main(int argc, char * argv[])
 {
 	/* check for errors */
 	if(argc != 2)
-	{
-		fprintf(stderr, "%s", "Usage: dirname string\n");
-		return 1;
-	}
+		return usage();
 	/* dirname */
 	printf("%s\n", dirname(argv[1]));
 	return 0;
