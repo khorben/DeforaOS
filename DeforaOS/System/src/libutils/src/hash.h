d570bb539f1d0c77aef3bd7962c1afb90e7342e5
khorben <>
2004-04-03 15:41:42 +0000

    Added hash_get_index() and hash_get_size()

--- DeforaOS/System/src/libutils/src/hash.h
+++ DeforaOS/System/src/libutils/src/hash.h
@@ -5,7 +5,7 @@
 #ifndef ___HASH_H
 # define ___HASH_H
 
-#include "darray.h"
+# include "darray.h"
 
 
 /* types */
@@ -14,8 +14,9 @@ typedef DArray Hash;
 
 /* functions */
 /* hash */
-Hash * hash_new();
+Hash * hash_new(void);
 void hash_delete(Hash * h);
+
 
 /* useful */
 /* hash_get
@@ -23,10 +24,19 @@ void hash_delete(Hash * h);
  * 		else	the corresponding data */
 void * hash_get(Hash * h, char * name);
 
+/* hash_get_index
+ * RETURNS	NULL	success or the corresponding data == NULL
+ * 		else	the corresponding data */
+void * hash_get_index(Hash * h, unsigned int index);
+
+/* hash_get_size
+ * RETURNS	any	the size of the hash table */
+unsigned int hash_get_size(Hash * h);
+
 /* hash_set
  * RETURNS	-1	failure
  * 		0	success */
 int hash_set(Hash * h, char * name, void * data);
 
 
-#endif /* ___HASH_H */
+#endif /* !___HASH_H */
