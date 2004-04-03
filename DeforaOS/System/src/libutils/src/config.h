6cfc13b2bb796ebb5ac545547222968305e4fa98
khorben <>
2004-04-03 15:42:37 +0000

    Depends on hash.h

--- DeforaOS/System/src/libutils/src/config.h
+++ DeforaOS/System/src/libutils/src/config.h
@@ -5,9 +5,17 @@
 #ifndef ___CONFIG_H
 # define ___CONFIG_H
 
+# include "hash.h"
+
 
 /* types */
 typedef struct _Config {
+	Hash * sections;
 } Config;
+
+
+/* functions */
+Config * config_new(void);
+void config_delete(Config * config);
 
 #endif /* !___CONFIG_H */
