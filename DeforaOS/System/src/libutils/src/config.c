de10fa3b61618f7a89e095cfea73e01a03b170f5
khorben <>
2004-04-03 15:41:08 +0000

    "Added symbols config_new() and config_delete()"

--- DeforaOS/System/src/libutils/src/config.c
+++ DeforaOS/System/src/libutils/src/config.c
@@ -2,7 +2,25 @@
 
 
 
+#include <stdlib.h>
 #include "config.h"
 
 
 /* Config */
+Config * config_new(void)
+{
+	Config * config;
+
+	if((config = malloc(sizeof(Config))) == NULL)
+		return NULL;
+	return config;
+}
+
+void config_delete(Config * config)
+{
+	int i;
+
+	for(i = hash_get_size(config->sections) - 1; i >= 0; i--)
+		hash_delete(hash_get_index(config->sections, i));
+	hash_delete(config->sections);
+}
