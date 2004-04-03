62583283fbeb3dd6a77c7b5f0cee1e61da1663ef
khorben <>
2004-04-03 15:12:09 +0000

    Initial release

---
+++ DeforaOS/System/src/libutils/src/common.h
@@ -0,0 +1,13 @@
+/* common.h */
+
+
+
+#ifndef ___COMMON_H
+# define ___COMMON_H
+
+/* macros */
+# define max(a, b) ((a > b) ? a : b)
+# define min(a, b) ((a < b) ? a : b)
+
+
+#endif /* !___COMMON_H */
