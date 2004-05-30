7c0666f6d6f9d95dccd614aea0d3a5c850aa0307
khorben <>
2004-05-30 11:57:15 +0000

    Preliminary support for loading configuration files (needs cleanup)

--- DeforaOS/Apps/Unix/src/gputty/src/config.c
+++ DeforaOS/Apps/Unix/src/gputty/src/config.c
@@ -19,6 +19,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "config.h"
 
 
@@ -65,9 +67,145 @@ int config_set(Config * config, char * section, char * variable, char * value)
 }
 
 
+static char * _load_section(FILE * fp);
+static char * _load_variable(FILE * fp, int c);
+static char * _load_value(FILE * fp);
 int config_load(Config * config, char * filename)
 {
-	return 1;
+	FILE * fp;
+	char * section;
+	char * variable = NULL;
+	int c;
+	char * str;
+	int ret;
+
+	if((section = strdup("")) == NULL)
+	{
+		fprintf(stderr, "%s", "gputty: ");
+		perror("strdup");
+		return 1;
+	}
+	if((fp = fopen(filename, "r")) == NULL)
+		return 1;
+	while((c = fgetc(fp)) != EOF)
+	{
+		if(c == '#')
+			while((c = fgetc(fp)) != EOF && 'c' != '\n');
+		else if(c == '[')
+		{
+			if((str = _load_section(fp)) == NULL)
+				break;
+			free(section);
+			section = str;
+		}
+		else if(isprint(c))
+		{
+			if((str = _load_variable(fp, c)) == NULL)
+				break;
+			free(variable);
+			variable = str;
+			if((str = _load_value(fp)) == NULL)
+				break;
+#ifdef DEBUG
+			fprintf(stderr, "%s%p, %s, %s, %s%s",
+					"config_set(", config,
+					section, variable, str, ");\n");
+#endif
+			config_set(config, section, variable, str);
+		}
+		else if(c != '\n')
+			break;
+	}
+	free(section);
+	free(variable);
+	if(!feof(fp))
+	{
+		ret = 1;
+		fprintf(stderr, "%s", "gputty: error in configuration file\n");
+	}
+	fclose(fp);
+	return ret;
+}
+
+static char * _load_section(FILE * fp)
+{
+	int c;
+	char * str = NULL;
+	int len = 1;
+	char * p;
+
+	while((c = fgetc(fp)) != EOF && c != ']' && isprint(c))
+	{
+		if((p = realloc(str, len + 1)) == NULL)
+		{
+			free(str);
+			return NULL;
+		}
+		str = p;
+		str[len++ - 1] = c;
+	}
+	if(c != ']')
+	{
+		free(str);
+		return NULL;
+	}
+	str[len - 1] = '\0';
+	return str;
+}
+
+static char * _load_variable(FILE * fp, int c)
+{
+	char * str;
+	int len = 2;
+	char * p;
+
+	if((str = malloc(len)) == NULL)
+		return NULL;
+	str[0] = c;
+	while((c = fgetc(fp)) != EOF && c != '=' && isprint(c))
+	{
+		if((p = realloc(str, len + 1)) == NULL)
+		{
+			free(str);
+			return NULL;
+		}
+		str = p;
+		str[len++ - 1] = c;
+	}
+	if(c != '=')
+	{
+		fprintf(stderr, "%d ici\n", c);
+		free(str);
+		return NULL;
+	}
+	str[len - 1] = '\0';
+	return str;
+}
+
+static char * _load_value(FILE * fp)
+{
+	int c;
+	char * str = NULL;
+	int len = 1;
+	char * p;
+
+	while((c = fgetc(fp)) != EOF && isprint(c))
+	{
+		if((p = realloc(str, len + 1)) == NULL)
+		{
+			free(str);
+			return NULL;
+		}
+		str = p;
+		str[len++ - 1] = c;
+	}
+	if(c != '\n')
+	{
+		free(str);
+		return NULL;
+	}
+	str[len - 1] = '\0';
+	return str;
 }
 
 /* FIXME */
