90aef28c0d4ef294a54c1c3e2c65b20e3c50a065
khorben <>
2005-01-18 23:18:33 +0000

    Corrected grammar

--- DeforaOS/Apps/Servers/src/inetd/src/parser.c
+++ DeforaOS/Apps/Servers/src/inetd/src/parser.c
@@ -40,10 +40,80 @@ static void _config(State * state)
 		_newline(state); */
 }
 
+static void _space(State * state);
 static void _newline(State * state)
+	/* [ space ] NEWLINE */
+{
+	if(token_in_set(state->token, TS_SPACE))
+		_space(state);
+	if(_parser_check(state, TC_NEWLINE))
+		state->line++;
+}
+
+static void _space(State * state)
+	/* SPACE { SPACE } */
+{
+	_parser_check(state, TC_SPACE);
+	while(state->token->code == TC_SPACE)
+		_parser_scan(state);
+}
+
+static void _service(State * state);
+static void _service_list(State * state)
+	/* { service { newline } } */
+{
+	while(token_in_set(state->token, TS_SERVICE))
+	{
+		_service(state);
+		while(token_in_set(state->token, TS_NEWLINE))
+			_newline(state);
+	}
+}
+
+static void _service_name(State * state);
+static void _socket(State * state);
+static void _protocol(State * state);
+static void _wait(State * state);
+static void _id(State * state);
+static void _program(State * state);
+static void _service(State * state)
+	/* service_name space socket space protocol space wait space
+	 * id space program newline */
+{
+	_service_name(state);
+	_space(state);
+	_socket(state);
+	_space(state);
+	_protocol(state);
+	_space(state);
+	_wait(state);
+	_space(state);
+	_id(state);
+	_space(state);
+	_program(state);
+	_newline(state);
+}
+
+static void _service_name(State * state);
 {
 }
 
-static void _service_list(State * state)
+static void _socket(State * state);
+{
+}
+
+static void _protocol(State * state);
+{
+}
+
+static void _wait(State * state);
+{
+}
+
+static void _id(State * state);
+{
+}
+
+static void _program(State * state)
 {
 }
