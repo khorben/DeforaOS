0962963694bb3b704b634bbfb1be2d41f561d5cc
khorben <>
2005-04-04 16:38:04 +0000

    Added doctype

--- DeforaOS/Apps/Web/PHP Explorer/tree.php
+++ DeforaOS/Apps/Web/PHP Explorer/tree.php
@@ -104,7 +104,9 @@ function tree()
 }
 
 ?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title>Tree</title>
 		<link type="text/css" rel="stylesheet" href="explorer.css"/>
