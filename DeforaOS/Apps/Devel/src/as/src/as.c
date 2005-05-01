a2816538cc763ce9a7442b8fa1352e6e4631f082
khorben <>
2005-05-01 18:13:16 +0000

    Result file is executable, or removed upon error (mode should use user's umask though)

--- DeforaOS/Apps/Devel/src/as/src/as.c
+++ DeforaOS/Apps/Devel/src/as/src/as.c
@@ -2,6 +2,8 @@
 
 
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
 #include "parser.h"
@@ -17,6 +19,7 @@ static int as(int prefs, char * infile, char * outfile)
 	FILE * infp;
 	FILE * outfp;
 	int ret;
+	int fd;
 
 	if((infp = fopen(infile, "r")) == NULL)
 		return _as_error(infile, 1);
@@ -27,7 +30,13 @@ static int as(int prefs, char * infile, char * outfile)
 	}
 	ret = _as_do(prefs, infile, infp, outfile, outfp);
 	fclose(infp);
+	if(ret == 0 && (fd = fileno(outfp)) != -1)
+		if(fchmod(fd, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+			ret = _as_error(outfile, 2);
 	fclose(outfp);
+	if(ret != 0)
+		if(unlink(outfile) != 0)
+			_as_error(outfile, 0);
 	return ret;
 }
 
