3f9c7a413e5c773b938415d599773b6530c8316f
khorben <>
2005-05-08 16:27:49 +0000

    Added type to handle registers

--- DeforaOS/Apps/Devel/src/as/src/arch/arch.h
+++ DeforaOS/Apps/Devel/src/as/src/arch/arch.h
@@ -11,12 +11,21 @@ typedef struct _ArchInstruction {
 	char * string;
 	unsigned int opcode;
 	unsigned int operand_count;
+	/* FIXME type of data relevant to this instruction */
 } ArchInstruction;
 
 typedef ArchInstruction * ArchInstructionSet;
 
+typedef struct _ArchRegister {
+	char * name;
+	unsigned int size;
+} ArchRegister;
+
+typedef ArchRegister * ArchRegisterSet;
+
 typedef struct _Arch {
 	ArchInstructionSet instructions;
+	ArchRegisterSet registers;
 } Arch;
 
 #endif /* !__ARCH_H */
