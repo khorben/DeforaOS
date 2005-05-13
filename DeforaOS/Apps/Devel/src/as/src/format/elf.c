478a804a7c661b5c063e06838e842417d76b2f6f
khorben <>
2005-05-13 21:56:07 +0000

    Sticking to format plug-in specifications and outputting (invalid and static) ELF header

--- DeforaOS/Apps/Devel/src/as/src/format/elf.c
+++ DeforaOS/Apps/Devel/src/as/src/format/elf.c
@@ -2,7 +2,34 @@
 
 
 
+#include <elf.h>
+#include <string.h>
 #include "format.h"
 
 
-FormatPlugin format_plugin;
+/* Format */
+int elf_init(FILE * fp)
+{
+	Elf32_Ehdr hdr;
+
+	strcpy(&hdr.e_ident, ELFMAG);
+	hdr.e_ident[4] = ELFCLASS32;	/* FIXME */
+	hdr.e_ident[5] = ELFDATA2LSB;	/* FIXME */
+	hdr.e_type = ET_REL;		/* FIXME */
+	hdr.e_machine = EM_386;		/* FIXME */
+	hdr.e_version = EV_CURRENT;	/* FIXME */
+	hdr.e_flags = EF_CPU32;		/* FIXME */
+	return fwrite(&hdr, sizeof(hdr), 1, fp) == 1 ? 0 : 1;
+}
+
+
+int elf_exit(void)
+{
+	return 0; /* FIXME */
+}
+
+
+FormatPlugin format_plugin = {
+	elf_init,
+	elf_exit
+};
