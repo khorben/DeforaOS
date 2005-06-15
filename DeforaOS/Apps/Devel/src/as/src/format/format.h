1aaba17cad87b0e3a76540bc7b844e3146d531e9
khorben <>
2005-06-15 01:38:28 +0000

    Found a solution for the format plug-ins architecture issues

--- DeforaOS/Apps/Devel/src/as/src/format/format.h
+++ DeforaOS/Apps/Devel/src/as/src/format/format.h
@@ -1,4 +1,8 @@
 /* format/format.h */
+/* FIXME
+ * - get rid of format_init() and format_exit() (should be handled in
+ *   format_new() and format_delete()
+ * - format plug-ins should be told and aware of the target architecture */
 
 
 
@@ -31,5 +35,7 @@ void format_delete(Format * format);
 /* useful */
 int format_init(Format * format, FILE * fp);
 int format_exit(Format * format, FILE * fp);
+
+int format_section(Format * format, char * section);
 
 #endif /* !__FORMAT_FORMAT_H */
