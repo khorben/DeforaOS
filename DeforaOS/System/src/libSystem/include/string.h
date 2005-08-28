94e3e04a19d62f92dd2472c7c592cd0973b2d7ce
khorben <>
2005-08-28 11:25:28 +0000

    Initial release

---
+++ DeforaOS/System/src/libSystem/include/string.h
@@ -0,0 +1,20 @@
+/* string.h */
+
+
+
+#ifndef _STRING_H
+# define _STRING_H
+
+
+/* String */
+typedef char String;
+
+String * string_new(String * string);
+void string_delete(String * string);
+
+/* useful */
+int string_append(String * string, String * append);
+void string_cut(String * string, unsigned int length);
+int string_length(String * string);
+
+#endif /* !_STRING_H */
