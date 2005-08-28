dbd6b136fac47b7ab6de0c92e0beef2e5016a4d0
khorben <>
2005-08-28 01:13:24 +0000

    appserver_new needs a supported service name

--- DeforaOS/System/src/libSystem/include/appserver.h
+++ DeforaOS/System/src/libSystem/include/appserver.h
@@ -14,8 +14,8 @@ typedef struct _AppServer AppServer;
 
 
 /* functions */
-AppServer * appserver_new(void);
-AppServer * appserver_new_event(Event * event);
+AppServer * appserver_new(const char * app);
+AppServer * appserver_new_event(const char * app, Event * event);
 void appserver_delete(AppServer * appserver);
 
 #endif /* !_APPSERVER_H */
