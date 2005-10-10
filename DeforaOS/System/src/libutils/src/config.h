0a2287df4f85b4f13d1e36cc4387add984d06b9d
khorben <>
2005-10-10 22:21:23 +0000

    Fixes memory uncertainties (variable values) Checks errors upon config save

--- DeforaOS/System/src/libutils/src/config.h
+++ DeforaOS/System/src/libutils/src/config.h
@@ -19,7 +19,8 @@ void config_delete(Config * config);
 
 /* useful */
 char * config_get(Config * config, char * section, char * variable);
-int config_set(Config * config, char const * section, char const * variable, char * value);
+int config_set(Config * config, char const * section, char const * variable,
+		char const * value);
 
 int config_load(Config * config, char * filename);
 int config_save(Config * config, char * filename);
