f2b48eff3114fe73c31b4c5ad6c31c59b05cb87b
khorben <>
2005-10-11 18:08:42 +0000

    Added users count

--- DeforaOS/Apps/Network/src/Probe/src/probe.c
+++ DeforaOS/Apps/Network/src/Probe/src/probe.c
@@ -3,6 +3,7 @@
 
 
 #include <System.h>
+#include <utmpx.h>
 #include <stdio.h>
 #ifdef _GNU_SOURCE
 # include <sys/sysinfo.h>
@@ -110,19 +111,24 @@ static int sysinfo(struct sysinfo * info)
 	return ret;
 }
 #endif
-struct sysinfo info;
 
 
 /* Probe */
+typedef struct _Probe
+{
+	struct sysinfo sysinfo;
+	unsigned int users;
+} Probe;
+Probe probe;
 static int _probe_error(char * message, int ret);
-static int _probe_timeout(struct sysinfo * info);
+static int _probe_timeout(Probe * probe);
 static int _probe(void)
 {
 	AppServer * appserver;
 	Event * event;
 	struct timeval tv;
 
-	if(sysinfo(&info) != 0)
+	if(_probe_timeout(&probe) != 0)
 		return _probe_error("sysinfo", 2);
 	if((event = event_new()) == NULL)
 		return _probe_error("Event", 2);
@@ -135,7 +141,7 @@ static int _probe(void)
 	tv.tv_sec = PROBE_REFRESH;
 	tv.tv_usec = 0;
 	if(event_register_timeout(event, tv, (EventTimeoutFunc)_probe_timeout,
-			&info) != 0)
+			&probe) != 0)
 		_probe_error("timeout", 0);
 	else
 		event_loop(event);
@@ -151,15 +157,20 @@ static int _probe_error(char * message, int ret)
 	return ret;
 }
 
-static int _probe_timeout(struct sysinfo * info)
+static int _probe_timeout(Probe * probe)
 {
+	struct utmpx * ut;
 #ifdef DEBUG
 	static unsigned int count = 0;
 
 	fprintf(stderr, "%s%d%s", "_probe_timeout(", count++, ")\n");
 #endif
-	if(sysinfo(info) != 0)
-		return _probe_error("sysinfo", 0);
+	if(sysinfo(&probe->sysinfo) != 0)
+		return _probe_error("sysinfo", 1);
+	for(probe->users = 0; (ut = getutxent()) != NULL;)
+		if(ut->ut_type == USER_PROCESS)
+			probe->users++;
+	endutxent();
 	return 0;
 }
 
@@ -167,72 +178,79 @@ static int _probe_timeout(struct sysinfo * info)
 /* AppInterface */
 int uptime(void)
 {
-	printf("%s%ld%s", "Uptime: ", info.uptime, "\n");
-	return info.uptime;
+	printf("%s%ld%s", "Uptime: ", probe.sysinfo.uptime, "\n");
+	return probe.sysinfo.uptime;
 }
 
 
 int load_1(void)
 {
-	printf("%s%lu%s", "Load 1: ", info.loads[0], "\n");
-	return info.loads[0];
+	printf("%s%lu%s", "Load 1: ", probe.sysinfo.loads[0], "\n");
+	return probe.sysinfo.loads[0];
 }
 
 
 int load_5(void)
 {
-	printf("%s%lu%s", "Load 5: ", info.loads[1], "\n");
-	return info.loads[1];
+	printf("%s%lu%s", "Load 5: ", probe.sysinfo.loads[1], "\n");
+	return probe.sysinfo.loads[1];
 }
 
 
 int load_15(void)
 {
-	printf("%s%lu%s", "Load 15: ", info.loads[2], "\n");
-	return info.loads[2];
+	printf("%s%lu%s", "Load 15: ", probe.sysinfo.loads[2], "\n");
+	return probe.sysinfo.loads[2];
 }
 
 
 int ram_total(void)
 {
-	printf("%s%lu%s", "Total RAM: ", info.totalram, "\n");
-	return info.totalram;
+	printf("%s%lu%s", "Total RAM: ", probe.sysinfo.totalram, "\n");
+	return probe.sysinfo.totalram;
 }
 
 int ram_free(void)
 {
-	printf("%s%lu%s", "Free RAM: ", info.freeram, "\n");
-	return info.freeram;
+	printf("%s%lu%s", "Free RAM: ", probe.sysinfo.freeram, "\n");
+	return probe.sysinfo.freeram;
 }
 
 int ram_shared(void)
 {
-	printf("%s%lu%s", "Shared RAM: ", info.sharedram, "\n");
-	return info.sharedram;
+	printf("%s%lu%s", "Shared RAM: ", probe.sysinfo.sharedram, "\n");
+	return probe.sysinfo.sharedram;
 }
 
 int ram_buffer(void)
 {
-	printf("%s%lu%s", "Buffered RAM: ", info.bufferram, "\n");
-	return info.bufferram;
+	printf("%s%lu%s", "Buffered RAM: ", probe.sysinfo.bufferram, "\n");
+	return probe.sysinfo.bufferram;
 }
 
 
 int swap_total(void)
 {
-	return info.totalswap;
+	return probe.sysinfo.totalswap;
 }
 
 int swap_free(void)
 {
-	return info.freeswap;
+	return probe.sysinfo.freeswap;
 }
 
 
 int procs(void)
 {
-	printf("%s%u%s", "Procs: ", info.procs, "\n");
-	return info.procs;
+	printf("%s%u%s", "Procs: ", probe.sysinfo.procs, "\n");
+	return probe.sysinfo.procs;
+}
+
+
+int users(void)
+{
+	printf("%s%u%s", "Users: ", probe.users, "\n");
+	return probe.users;
 }
 
 
