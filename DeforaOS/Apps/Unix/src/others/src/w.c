2d4a84881b51bce8aed53a3ca29f4097c319cfc4
khorben <>
2006-02-11 20:31:23 +0000

    Initial release

---
+++ DeforaOS/Apps/Unix/src/others/src/w.c
@@ -0,0 +1,110 @@
+/* w.c */
+
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
+#include <utmpx.h>
+#include <stdio.h>
+
+
+/* w */
+static int _w_error(char * message, int ret);
+static int _print_idle(struct timeval * tv, char * device);
+static int _print_what(pid_t pid);
+static int _w(void)
+{
+	struct utmpx * u;
+	struct timeval tv;
+
+	if(gettimeofday(&tv, NULL) != 0)
+		return _w_error("gettimeofday", 2);
+	printf("%-8s %-8s %-16s %-5s %5s %s\n", "USER", "TTY", "FROM",
+			"LOGIN@", "IDLE", "WHAT");
+	for(; (u = getutxent()) != NULL;)
+	{
+		if(u->ut_type != USER_PROCESS)
+			continue;
+		printf("%-8s %-8s %-16s  %2ld:%02ld ", u->ut_user, u->ut_line,
+				u->ut_host,
+				(u->ut_tv.tv_sec - u->ut_tv.tv_sec % 3600) % 24,
+				(u->ut_tv.tv_sec - u->ut_tv.tv_sec % 60) / 60
+				% 60);
+		_print_idle(&tv, u->ut_line);
+		_print_what(u->ut_pid);
+	}
+	return 0;
+}
+
+static int _w_error(char * message, int ret)
+{
+	fprintf(stderr, "%s", "w: ");
+	perror(message);
+	return ret;
+}
+
+static int _print_idle(struct timeval * tv, char * device)
+{
+	char dev[16];
+	struct stat st;
+	time_t idle;
+
+	if(snprintf(dev, sizeof(dev), "%s%s", "/dev/", device)
+			> (int)sizeof(dev))
+		return printf("%5s ", "?");
+	if(lstat(dev, &st) != 0)
+		return printf("%5s ", "?");
+	idle = tv->tv_sec - st.st_atime;
+	if(idle / 3600 >= 24)
+		return printf("%4ldd ", idle / 3600 / 24);
+	else if(idle / 60 >= 60)
+		return printf("%02ld:%02ld ", idle / 3600, idle % 3600 / 60);
+	return printf("%02ldm%02ld ", idle / 60, idle % 60);
+}
+
+static int _print_what(pid_t pid)
+{
+	char proc[33];
+	FILE * fp;
+	int len;
+
+	if(snprintf(proc, sizeof(proc), "%s%d%s", "/proc/", pid, "/cmdline")
+			> (int)sizeof(proc))
+		return printf(" %d\n", pid);
+	if((fp = fopen(proc, "r")) == NULL)
+	{
+		printf(" %d\n", pid);
+		return _w_error(proc, 0);
+	}
+	if((len = fread(proc, sizeof(char), sizeof(proc)-1, fp)) < 0)
+	{
+		fclose(fp);
+		printf(" %d\n", pid);
+		return _w_error(proc, 0);
+	}
+	proc[len] = '\0';
+	printf("%s\n", proc);
+	return fclose(fp);
+}
+
+
+/* usage */
+static int _usage(void)
+{
+	fprintf(stderr, "%s", "Usage: w\n");
+	return 1;
+}
+
+
+/* main */
+int main(int argc, char * argv[])
+{
+	int o;
+
+	while((o = getopt(argc, argv, "")) != -1)
+		return _usage();
+	return _w() == 0 ? 0 : 2;
+}
