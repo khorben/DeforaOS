f1dc344a92131655df6eb2db374e79ebe8d555b5
khorben <>
2006-03-14 20:52:05 +0000

    Improved bookmarks list

--- DeforaOS/Apps/Web/DaPortal/modules/bookmark/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/bookmark/module.php
@@ -91,22 +91,30 @@ function bookmark_list($args)
 		return _error(PERMISSION_DENIED);
 	print('<h1><img src="modules/bookmark/icon.png" alt=""/> '
 			._html_safe(BOOKMARK_LIST).'</h1>'."\n");
-	$bookmarks = _sql_array('SELECT bookmark_id AS id, title, content, url'
+	$bookmarks = _sql_array('SELECT bookmark_id AS id, title AS name, content, url'
 			.' FROM daportal_bookmark, daportal_content'
 			.' WHERE daportal_bookmark.bookmark_id'
 			.'=daportal_content.content_id'
 			." AND user_id='$user_id';");
 	if(!is_array($bookmarks))
 		return _error('Could not list bookmarks');
+	for($i = 0, $count = count($bookmarks); $i < $count; $i++)
+	{
+		$bookmarks[$i]['module'] = 'bookmark';
+		$bookmarks[$i]['action'] = 'display';
+		$bookmarks[$i]['url'] = '<a href="'
+			._html_safe_link($bookmarks[$i]['url']).'">'
+			._html_safe($bookmarks[$i]['url'])."</a>";
+	}
 	$toolbar = array();
 	$toolbar[] = array('title' => NEW_BOOKMARK,
 			'icon' => 'modules/bookmark/icon.png',
 			'link' => 'index.php?module=bookmark&action=new');
-	_module('explorer', 'browse', array(
-			'class' => array('url' => ADDRESS),
-			'view' => 'details',
-			'toolbar' => $toolbar,
-			'entries' => $bookmarks));
+	_module('explorer', 'browse_trusted', array(
+				'class' => array('url' => ADDRESS),
+				'view' => 'details',
+				'toolbar' => $toolbar,
+				'entries' => $bookmarks));
 }
 
 
