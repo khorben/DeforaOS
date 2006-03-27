4205a2481746051e9d4a287e536bb28442ec46d8
khorben <>
2006-03-27 20:17:48 +0000

    Implemented almost every required functionality

--- DeforaOS/Apps/Web/DaPortal/modules/category/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/category/module.php
@@ -8,10 +8,13 @@ if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
 
 
 //lang
+$text['ALREADY_LINKED'] = 'Already linked';
 $text['CATEGORIES_ADMINISTRATION'] = 'Categories administration';
 $text['CATEGORIES_LIST'] = 'Categories list';
 $text['CATEGORY'] = 'Category';
-$text['NEW_CATEGORY'] = ' New category';
+$text['DELETE_LINK'] = 'Delete link';
+$text['MEMBER_OF'] = 'Member of';
+$text['NEW_CATEGORY'] = 'New category';
 global $lang;
 _lang($text);
 
@@ -41,6 +44,7 @@ function category_admin($args)
 				.$categories[$i]['enabled'].'" title="'
 				.($categories[$i]['enabled'] == 'enabled'
 						? ENABLED : DISABLED).'"/>';
+		/* FIXME do everything in one query */
 		$categories[$i]['members'] = _sql_single('SELECT COUNT(*)'
 				.' FROM daportal_category_content'
 				." WHERE category_id='".$categories[$i]['id']
@@ -59,6 +63,10 @@ function category_admin($args)
 	$toolbar[] = array('title' => DISABLE,
 			'icon' => 'icons/16x16/disabled.png',
 			'action' => 'disable');
+	$toolbar[] = array('title' => DELETE,
+			'icon' => 'icons/16x16/delete.png',
+			'action' => 'delete',
+			'confirm' => 'delete');
 	_module('explorer', 'browse_trusted', array('entries' => $categories,
 				'class' => array('enabled' => ENABLED,
 					'members' => MEMBERS),
@@ -89,6 +97,20 @@ function category_disable($args)
 }
 
 
+function category_delete($args)
+{
+	global $user_id;
+
+	require_once('system/user.php');
+	if(!_user_admin($user_id))
+		return _error(PERMISSION_DENIED);
+	_sql_query('DELETE FROM daportal_category_content'
+			." WHERE category_id='".$args['id']."';");
+	_sql_query('DELETE FROM daportal_content'
+			." WHERE content_id='".$args['id']."';");
+}
+
+
 function category_display($args)
 {
 	$module = _module_id('category');
@@ -98,7 +120,7 @@ function category_display($args)
 			." AND module_id='$module'"
 			." AND enabled='1';");
 	if(!is_array($category) || count($category) != 1)
-		return _error('Unable to display category', 1);
+		return _error('Unable to display category');
 	$category = $category[0];
 	$title = CATEGORY.' '.$category['title'];
 	include('display.tpl');
@@ -110,7 +132,7 @@ function category_display($args)
 			." AND enabled='1'"
 			." AND category_id='".$args['id']."';");
 	if(!is_array($contents))
-		return _error('Unable to display category', 1);
+		return _error('Unable to display category');
 	_module('explorer', 'browse', array('entries' => $contents));
 }
 
@@ -127,6 +149,64 @@ function category_enable($args)
 }
 
 
+function category_get($args)
+{
+	global $user_id;
+
+	if(_sql_single('SELECT name FROM daportal_content, daportal_module'
+			.' WHERE daportal_content.module_id'
+			.'=daportal_module.module_id'
+			." AND content_id='".$args['id']."';") == 'category')
+		return _error(INVALID_ARGUMENT);
+	print('<h2><img src="modules/category/icon.png" alt=""/> '
+			.MEMBER_OF.'</h2>'."\n");
+	$categories = _sql_array('SELECT category_id AS id, title AS name'
+			.' FROM daportal_category_content, daportal_content'
+			.' WHERE daportal_category_content.category_id'
+			.'=daportal_content.content_id'
+			." AND enabled='1'"
+			." AND daportal_category_content.content_id='"
+			.$args['id']."';");
+	if(!is_array($categories))
+		return _error('Could not list categories');
+	$count = count($categories);
+	for($i = 0; $i < $count; $i++)
+	{
+		$categories[$i]['module'] = 'category';
+		$categories[$i]['action'] = 'default';
+		$categories[$i]['icon'] = 'modules/category/icon.png';
+		$categories[$i]['thumbnail'] = 'modules/category/icon.png';
+		$categories[$i]['apply_module'] = 'category';
+		$categories[$i]['apply_id'] = $categories[$i]['id'];
+		$categories[$i]['apply_args'] = 'content_id='.$args['id'];
+	}
+	$toolbar = array();
+	$toolbar[] = array('title' => NEW_CATEGORY,
+			'icon' => 'modules/category/icon.png',
+			'link' => 'index.php?module=category&action=new');
+	$module = _sql_single('SELECT name'
+			.' FROM daportal_content, daportal_module'
+			.' WHERE daportal_content.module_id'
+			.'=daportal_module.module_id'
+			." AND daportal_content.enabled='1'"
+			." AND content_id='".$args['id']."';");
+	if($user_id != 0 && _sql_single('SELECT user_id FROM daportal_content'
+			." WHERE enabled='1' AND content_id='".$args['id']."';")
+			== $user_id)
+	{
+		$toolbar[] = array();
+		$toolbar[] = array('title' => DELETE_LINK,
+				'icon' => 'icons/16x16/delete.png',
+				'action' => 'link_delete',
+				'confirm' => 'delete link');
+	}
+	_module('explorer', 'browse', array('entries' => $categories,
+				'view' => 'list', 'toolbar' => $toolbar,
+				'module' => 'category', 'action' => 'set',
+				'id' => $args['id']));
+}
+
+
 function category_insert($args)
 {
 	global $user_id;
@@ -138,6 +218,63 @@ function category_insert($args)
 			== FALSE)
 		return _error('Unable to insert category');
 	category_display(array('id' => $id));
+}
+
+
+function category_link_delete($args)
+{
+	global $user_id;
+
+	if($args['id'] == 0 || $args['content_id'] == 0)
+		return _error(INVALID_ARGUMENT);
+	$module = _module_id('category');
+	if(_sql_single('SELECT module_id FROM daportal_content'
+			." WHERE enabled='1'"
+			." AND content_id='".$args['id']."';") != $module
+			|| _sql_single('SELECT module_id FROM daportal_content'
+				." WHERE enabled='1'"
+				." AND content_id='".$args['content_id']."';")
+			== $module)
+		return _error(INVALID_ARGUMENT);
+	if(_sql_single('SELECT user_id FROM daportal_content'
+			." WHERE enabled='1'"
+			." AND content_id='".$args['content_id']."';")
+			!= $user_id)
+		return _error(PERMISSION_DENIED);
+	_sql_query('DELETE FROM daportal_category_content WHERE'
+			." category_id='".$args['id']."'"
+			." AND content_id='".$args['content_id']."';");
+}
+
+
+function category_link_insert($args)
+{
+	global $user_id;
+
+	if($args['id'] == 0 || $args['content_id'] == 0)
+		return _error(INVALID_ARGUMENT);
+	$module = _module_id('category');
+	if(_sql_single('SELECT module_id FROM daportal_content'
+			." WHERE enabled='1'"
+			." AND content_id='".$args['id']."';") != $module
+			|| _sql_single('SELECT module_id FROM daportal_content'
+				." WHERE enabled='1'"
+				." AND content_id='".$args['content_id']."';")
+			== $module)
+		return _error(INVALID_ARGUMENT);
+	if(_sql_single('SELECT user_id FROM daportal_content'
+			." WHERE enabled='1'"
+			." AND content_id='".$args['content_id']."';")
+			!= $user_id)
+		return _error(PERMISSION_DENIED);
+	if(_sql_single('SELECT content_id FROM daportal_category_content'
+			." WHERE category_id='".$args['id']."'"
+			." AND content_id='".$args['content_id']."';")
+			== $args['content_id'])
+		return _error(ALREADY_LINKED);
+	_sql_query('INSERT INTO daportal_category_content'
+			.' (category_id, content_id) VALUES ('
+			."'".$args['id']."', '".$args['content_id']."');");
 }
 
 
@@ -198,6 +335,52 @@ function category_new($args)
 		return _error(PERMISSION_DENIED);
 	$title = NEW_CATEGORY;
 	include('update.tpl');
+}
+
+
+function category_set($args)
+{
+	global $user_id;
+
+	if($user_id == 0 || _sql_single('SELECT user_id FROM daportal_content'
+			." WHERE enabled='1' AND content_id='".$args['id']."';")
+			!= $user_id)
+		return _error(PERMISSION_DENIED);
+	$module = _module_id('category');
+	if(_sql_single('SELECT module_id FROM daportal_content'
+			." WHERE content_id='".$args['id']."';") == $module)
+		return _error(INVALID_ARGUMENT);
+	require_once('system/content.php');
+	if(!_content_display($args['id']))
+		return _error('Could not display content');
+	print('<h2><img src="modules/category/icon.png" alt=""/>'
+			.' Choose categories</h2>'."\n");
+	$categories = _sql_array('SELECT content_id AS id, title AS name'
+			.' FROM daportal_content'
+			." WHERE module_id='$module'"
+			." AND enabled='1'");
+	if(!is_array($categories))
+		return _error('Could not list categories');
+	$cnt = count($categories);
+	for($i = 0; $i < $cnt; $i++)
+	{
+		$categories[$i]['module'] = 'category';
+		$categories[$i]['action'] = 'display';
+		$categories[$i]['icon'] = 'modules/category/icon.png';
+		$categories[$i]['thumbnail'] = 'modules/category/icon.png';
+		$categories[$i]['apply_module'] = 'category';
+		$categories[$i]['apply_id'] = $categories[$i]['id'];
+		$categories[$i]['apply_args'] = 'content_id='.$args['id'];
+	}
+	$toolbar = array();
+	$toolbar[] = array('title' => 'Associate to categories',
+			'icon' => 'modules/category/icon.png',
+			'action' => 'link_insert');
+	_module('explorer', 'browse', array('entries' => $categories,
+				'view' => 'details', 'toolbar' => $toolbar,
+				'module' => 'category',
+				'action' => 'set',
+				'id' => $args['id']));
 }
 
 
