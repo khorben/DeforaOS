37a1c5786de440523e0818050ab1830786200fd3
khorben <>
2006-04-02 00:14:06 +0000

    Added target makedepend

---
+++ DeforaOS/Apps/Devel/src/configure/src/makedepend.c
@@ -0,0 +1,11 @@
+/* makedepend.c */
+/* TODO:
+ * - parse C files (.c and .h) and update dependencies in project.conf */
+
+
+/* main */
+int main(int argc, char * argv[])
+{
+	/* FIXME */
+	return 1;
+}
