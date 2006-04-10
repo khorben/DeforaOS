9a6cc5ba0af42bf8427f89e2e021321687cfa37f
khorben <>
2006-04-10 22:19:42 +0000

    Uses global translation mechanism

--- DeforaOS/Apps/Web/DaPortal/modules/article/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/article/desktop.php
@@ -6,12 +6,17 @@
 if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
 	exit(header('Location: ../../index.php'));
 
+//lang
+$text['ARTICLES'] = 'Articles';
+$text['SUBMIT'] = 'Submit';
+global $lang;
+_lang($text);
 
-$title = 'Articles';
+$title = ARTICLES;
 $admin = 1;
 $list = 1;
 if($user_id)
-	$actions = array('submit' => 'Submit');
+	$actions = array('submit' => SUBMIT);
 
 
 ?>
