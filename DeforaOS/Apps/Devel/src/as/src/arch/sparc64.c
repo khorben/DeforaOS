027a4c4b65e62a0c65c94216ecfa1ea062274003
khorben <>
2006-04-14 19:42:33 +0000

    Added sparc64 architecture

---
+++ DeforaOS/Apps/Devel/src/as/src/arch/sparc64.c
@@ -0,0 +1,21 @@
+/* sparc64.c */
+
+
+
+#include "arch.h"
+
+
+/* variables */
+ArchInstruction arch_sparc64_set[] =
+{
+};
+
+ArchRegister arch_sparc64_regs[] =
+{
+};
+
+ArchPlugin arch_sparc64 =
+{
+	arch_sparc64_regs,
+	arch_sparc64_set
+};
