1fe3d5b0f01b13da56523002513c417030894c18
khorben <>
2006-04-14 22:06:10 +0000

    Valid plug-ins

--- DeforaOS/Apps/Devel/src/as/src/arch/sparc.c
+++ DeforaOS/Apps/Devel/src/as/src/arch/sparc.c
@@ -2,19 +2,22 @@
 
 
 
+#include <stdlib.h>
 #include "arch.h"
 
 
 /* variables */
-ArchInstruction arch_sparc_set[] =
-{
-};
-
 ArchRegister arch_sparc_regs[] =
 {
+	{ NULL,	0,	0 }
 };
 
-ArchPlugin arch_sparc =
+ArchInstruction arch_sparc_set[] =
+{
+	{ NULL,	0x0,	0, 0, 0, 0 }
+};
+
+ArchPlugin arch_plugin =
 {
 	arch_sparc_regs,
 	arch_sparc_set
--- DeforaOS/Apps/Devel/src/as/src/arch/sparc64.c
+++ DeforaOS/Apps/Devel/src/as/src/arch/sparc64.c
@@ -2,19 +2,22 @@
 
 
 
+#include <stdlib.h>
 #include "arch.h"
 
 
 /* variables */
-ArchInstruction arch_sparc64_set[] =
-{
-};
-
 ArchRegister arch_sparc64_regs[] =
 {
+	{ NULL,	0,	0 }
 };
 
-ArchPlugin arch_sparc64 =
+ArchInstruction arch_sparc64_set[] =
+{
+	{ NULL,	0x0,	0, 0, 0, 0 }
+};
+
+ArchPlugin arch_plugin =
 {
 	arch_sparc64_regs,
 	arch_sparc64_set
