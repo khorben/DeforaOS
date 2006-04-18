723f31ada6d3034fcd84b09a3cbd89d02a8219fe
khorben <>
2006-04-18 21:40:23 +0000

    Implemented ELF64 output

--- DeforaOS/Apps/Devel/src/as/src/format/elf.c
+++ DeforaOS/Apps/Devel/src/as/src/format/elf.c
@@ -35,7 +35,7 @@ static FormatArch _elf_endian[] =
 typedef struct _SectionValues
 {
 	char * section;
-	Elf32_Word type;
+	Elf32_Word type;	/* works for 64-bit too */
 	Elf32_Word flags;
 } SectionValues;
 
@@ -66,6 +66,8 @@ static SectionValues _elf_section[] =
 	{ NULL,		0,		0				}
 };
 
+static FormatArch * fa;
+
 
 /* Format */
 static int _elf_error(char * message, int ret)
@@ -76,16 +78,35 @@ static int _elf_error(char * message, int ret)
 }
 
 static FormatArch * _init_arch(char * arch);
+static int _init_32(FILE * fp);
+static int _init_64(FILE * fp);
 int elf_init(FILE * fp, char * arch)
 {
-	Elf32_Ehdr hdr;
-	FormatArch * fa;
-
 	if((fa = _init_arch(arch)) == NULL)
 		return 1;
+	if(fa->capacity == ELFCLASS64)
+		return _init_64(fp);
+	return _init_32(fp);
+}
+
+static FormatArch * _init_arch(char * arch)
+{
+	unsigned int i;
+
+	for(i = 0; _elf_endian[i].arch != NULL; i++)
+		if(strcmp(_elf_endian[i].arch, arch) == 0)
+			return &_elf_endian[i];
+	fprintf(stderr, "%s", "as: ELF format: Unsupported architecture\n");
+	return NULL;
+}
+
+static int _init_32(FILE * fp)
+{
+	Elf32_Ehdr hdr;
+
 	memset(&hdr, 0, sizeof(hdr));
 	memcpy(&hdr.e_ident, ELFMAG, SELFMAG);
-	hdr.e_ident[EI_CLASS] = fa->capacity;
+	hdr.e_ident[EI_CLASS] = ELFCLASS32;
 	hdr.e_ident[EI_DATA] = fa->endian;
 	hdr.e_ident[EI_VERSION] = EV_CURRENT;
 	hdr.e_type = ET_REL;
@@ -104,36 +125,59 @@ int elf_init(FILE * fp, char * arch)
 	return fwrite(&hdr, sizeof(hdr), 1, fp) == 1 ? 0 : 1;
 }
 
-static FormatArch * _init_arch(char * arch)
+static int _init_64(FILE * fp)
 {
-	unsigned int i;
+	Elf64_Ehdr hdr;
 
-	for(i = 0; _elf_endian[i].arch != NULL; i++)
-		if(strcmp(_elf_endian[i].arch, arch) == 0)
-			return &_elf_endian[i];
-	fprintf(stderr, "%s", "as: ELF format: Unsupported architecture\n");
-	return NULL;
+	memset(&hdr, 0, sizeof(hdr));
+	memcpy(&hdr.e_ident, ELFMAG, SELFMAG);
+	hdr.e_ident[EI_CLASS] = ELFCLASS64;
+	hdr.e_ident[EI_DATA] = fa->endian;
+	hdr.e_ident[EI_VERSION] = EV_CURRENT;
+	hdr.e_type = ET_REL;
+	hdr.e_machine = fa->machine;
+	hdr.e_version = EV_CURRENT;
+	hdr.e_entry = 0;
+	hdr.e_phoff = 0;
+	hdr.e_shoff = 0;
+	hdr.e_flags = 0;
+	hdr.e_ehsize = sizeof(hdr);
+	hdr.e_phentsize = 0;
+	hdr.e_phnum = 0;
+	hdr.e_shentsize = sizeof(Elf64_Shdr);
+	hdr.e_shnum = 0;
+	hdr.e_shstrndx = SHN_UNDEF;
+	return fwrite(&hdr, sizeof(hdr), 1, fp) == 1 ? 0 : 1;
 }
 
 
 static int _section_update(FILE * fp, char * section);
 int elf_exit(FILE * fp)
 {
-	_section_update(fp, NULL);
-	return 0; /* FIXME */
+	return _section_update(fp, NULL);
 }
 
 
 static SectionValues * _section_values(char * section);
+static int _section_64(FILE * fp, SectionValues * sv);
+static int _section_32(FILE * fp, SectionValues * sv);
 int elf_section(FILE * fp, char * section)
 {
-	Elf32_Shdr hdr;
 	SectionValues * sv;
 
 	if(_section_update(fp, section) != 0)
 		return 1;
-	memset(&hdr, 0, sizeof(hdr));
 	sv = _section_values(section);
+	if(fa->capacity == ELFCLASS64)
+		return _section_64(fp, sv);
+	return _section_32(fp, sv);
+}
+
+static int _section_64(FILE * fp, SectionValues * sv)
+{
+	Elf64_Shdr hdr;
+
+	memset(&hdr, 0, sizeof(hdr));
 	hdr.sh_name = 0;
 	hdr.sh_type = sv->type;
 	hdr.sh_flags = sv->flags;
@@ -146,7 +190,76 @@ int elf_section(FILE * fp, char * section)
 	return fwrite(&hdr, sizeof(hdr), 1, fp) == 1 ? 0 : 1;
 }
 
+static int _section_32(FILE * fp, SectionValues * sv)
+{
+	Elf32_Shdr hdr;
+
+	memset(&hdr, 0, sizeof(hdr));
+	hdr.sh_name = 0;
+	hdr.sh_type = sv->type;
+	hdr.sh_flags = sv->flags;
+	hdr.sh_addr = 0;
+	hdr.sh_offset = ftell(fp);
+	hdr.sh_link = SHN_UNDEF;
+	hdr.sh_info = 0;
+	hdr.sh_addralign = 0;
+	hdr.sh_entsize = 0;
+	return fwrite(&hdr, sizeof(hdr), 1, fp) == 1 ? 0 : 1;
+}
+
+static int _update_64(FILE * fp, char * section);
+static int _update_32(FILE * fp, char * section);
 static int _section_update(FILE * fp, char * section)
+{
+	if(fa->capacity == ELFCLASS64)
+		return _update_64(fp, section);
+	return _update_32(fp, section);
+}
+
+static int _update_64(FILE * fp, char * section)
+{
+	Elf64_Shdr hdr;
+	static char * current = NULL;
+	long offset = sizeof(Elf64_Ehdr);
+	long size = 0;
+
+	if(current == NULL)
+	{
+		if(section == NULL)
+			return 0; /* FIXME mandatory sections? */
+		if((offset = ftell(fp)) == -1
+				|| (current = strdup(section)) == NULL)
+			return 1;
+		return 0;
+	}
+	if((size = ftell(fp)) == -1)
+		return _elf_error(section, 1);
+	size -= offset + sizeof(hdr);
+	if(fseek(fp, offset, SEEK_SET) != 0)
+		return _elf_error(section, 1); /* FIXME free memory if section == NULL */
+	if(fread(&hdr, sizeof(hdr), 1, fp) != 1)
+		return _elf_error(section, 1);
+	hdr.sh_size = size;
+	if(fseek(fp, offset, SEEK_SET) != 0)
+		return _elf_error(section, 1); /* FIXME free memory if section == NULL */
+	/* FIXME update the section structure */
+	if(fwrite(&hdr, sizeof(hdr), 1, fp) != 1)
+		return _elf_error(section, 1);
+	if(fseek(fp, 0, SEEK_END) != 0)
+		return _elf_error(section, 1); /* FIXME free memory if section == NULL */
+	if(section == NULL)
+	{
+		free(current);
+		current = NULL;
+		return 0;
+	}
+	if((offset = ftell(fp)) == -1
+			|| (current = strdup(section)) == NULL)
+		return _elf_error(section, 1);
+	return 0;
+}
+
+static int _update_32(FILE * fp, char * section)
 {
 	Elf32_Shdr hdr;
 	static char * current = NULL;
