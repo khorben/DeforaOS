b5399e092a8a4c0be18b4c30f13f07d26854301d
khorben <>
2006-04-30 16:57:37 +0000

    Syntax highlighting private functions are in a separate file

---
+++ DeforaOS/Apps/Web/DaPortal/modules/project/syntax.php
@@ -0,0 +1,99 @@
+<?php //modules/project/syntax.php
+
+//check url
+if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
+	exit(header('Location: ../../index.php'));
+
+function _file_csrc($line)
+{
+	static $comment = 0;
+
+	$line = preg_replace('/(&quot;[^(&quot;)]+&quot;)/',
+			'<span class="string">\1</span>', $line);
+	$line = preg_replace("/('(\\\\?.|[^'])')/",
+			'<span class="character">\1</span>', $line);
+	if($line[0] == '#')
+	{
+		$line = '<span class="preprocessed">'.substr($line, 0, -1)
+			.'</span>'."\n";
+		return $line;
+	}
+	$line = preg_replace('/(^|[^a-zA-Z0-9_])(break|case|continue|default'
+				.'|do|else|for|goto|if|return|sizeof|switch'
+				.'|until|while'
+				.')($|[^a-zA-Z0-9_])/',
+			'\1<span class="keyword">\2</span>\3', $line);
+	$line = preg_replace('/(^|[^a-zA-Z0-9_])('
+				.'char|DIR|double|FILE|float|int'
+				.'|long|short|size_t|ssize_t|signed|static'
+				.'|struct|typedef|union|unsigned|va_list|void'
+				.')($|[^a-zA-Z0-9_])/',
+			'\1<span class="type">\2</span>\3', $line);
+	/* FIXME fails on quoted strings, use prefix.line.suffix to escape hl */
+	if($comment == 0 && strstr($line, '/*'))
+	{
+		$p = strpos($line, '/*');
+		$line = substr($line, 0, $p).'<span class="comment">'
+			.substr($line, $p);
+		$comment = 1;
+	}
+	if($comment != 0 && strstr($line, '*/'))
+	{
+		$p = strpos($line, '*/');
+		$line = substr($line, 0, $p+2).'</span>'.substr($line, $p+2);
+		$comment = 0;
+	}
+	return $line;
+}
+
+function _file_makefile($line)
+{
+	$line = preg_replace('/\s(#.*$)/',
+			'<span class="comment">\1</span>', $line);
+	$line = preg_replace('/^([a-zA-Z]+)(\s*=)/',
+			'<span class="variable">\1</span>\2', $line);
+	$line = preg_replace('/(^[a-zA-Z]+:)/',
+			'<span class="variable">\1</span>', $line);
+	$line = preg_replace('/(\$\([a-zA-Z_]+\))/',
+			'<span class="variable">\1</span>', $line);
+	return $line;
+}
+
+function _file_php($line)
+{
+	static $comment = 0;
+
+	$line = preg_replace('/(&quot;[^(&quot;)]+&quot;)/',
+			'<span class="string">\1</span>', $line);
+	$line = preg_replace("/('(.|[^']*)')/",
+			'<span class="string">\1</span>', $line);
+	$line = preg_replace('/(^|[^a-zA-Z0-9_])(break|case|continue|default'
+				.'|do|else|for|foreach|if|return|switch'
+				.'|until|while|=&gt;|&amp;&amp;|\|\|'
+				.')($|[^a-zA-Z0-9_])/',
+			'\1<span class="keyword">\2</span>\3', $line);
+	$line = preg_replace('/(^|[^a-zA-Z0-9_])('
+				.'array|function|global|include|include_once'
+				.'|require|require_once|static'
+				.')($|[^a-zA-Z0-9_])/',
+			'\1<span class="type">\2</span>\3', $line);
+	$line = preg_replace('/(\/\/.*$)/',
+			'<span class="comment">\1</span>', $line);
+	/* FIXME fails on quoted strings, use prefix.line.suffix to escape hl */
+	if($comment == 0 && strstr($line, '/*'))
+	{
+		$p = strpos($line, '/*');
+		$line = substr($line, 0, $p).'<span class="comment">'
+			.substr($line, $p);
+		$comment = 1;
+	}
+	if($comment != 0 && strstr($line, '*/'))
+	{
+		$p = strpos($line, '*/');
+		$line = substr($line, 0, $p+2).'</span>'.substr($line, $p+2);
+		$comment = 0;
+	}
+	return $line;
+}
+
+?>
