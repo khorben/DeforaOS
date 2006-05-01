ecdf30957b0d4c90e800face5dbaa7f2e4334cd0
khorben <>
2006-05-01 13:06:20 +0000

    Probably faster code, added page execution duration measurement

--- DeforaOS/Apps/Web/DaPortal/system/debug.php
+++ DeforaOS/Apps/Web/DaPortal/system/debug.php
@@ -8,16 +8,27 @@ if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
 
 
 $debug_messages = '';
+$debug_time_before = gettimeofday();
 
 
 function _debug()
 {
-	global $debug, $html, $debug_messages;
+	global $debug, $html, $debug_messages, $debug_time_before;
 
 	if(!$debug || !$html)
 		return;
 	print('<div class="debug"><div class="system">'."\n");
 	print($debug_messages);
+	$debug_time_after = gettimeofday();
+	$sec = $debug_time_after['sec'] - $debug_time_before['sec'];
+	$usec = $debug_time_after['usec'] - $debug_time_before['usec'];
+	if($usec < 0)
+	{
+		$usec = $debug_time_before['usec'] - $debug_time_after['usec'];
+		$sec++;
+	}
+	print('<div class="info"><b>Info:</b> Page execution duration: '
+			.$sec.'s and '.ceil($usec/1000).'ms</div>'."\n");
 	print('</div></div>'."\n");
 }
 
@@ -26,15 +37,19 @@ function _debug()
 //	$level is trusted
 function _debug_message($level, $message, $visible = 0)
 {
-	global $debug_messages, $html;
+	global $debug, $debug_messages, $html;
 
-	$debug_messages.="\t".'<div class="'.$level.'"><b>'.(ucfirst($level))
-			.':</b> '.htmlspecialchars($message)."</div>\n";
-	if($visible && $html)
+	if(!$html)
+		return;
+	if($visible)
 		print('<div class="debug"><div class="visible error">'
 				.'<img src="images/'.$level.'.png"'
 				.' alt="error"/> '.htmlspecialchars($message)
 				."</div></div>\n");
+	if(!$debug)
+		return;
+	$debug_messages.="\t".'<div class="'.$level.'"><b>'.(ucfirst($level))
+			.':</b> '.htmlspecialchars($message)."</div>\n";
 }
 
 
