6254907a22849e7b10b735099dcf15504a0a7c78
khorben <>
2006-05-04 11:58:59 +0000

    Simplified plugin

--- DeforaOS/Apps/Devel/src/as/src/format/flat.c
+++ DeforaOS/Apps/Devel/src/as/src/format/flat.c
@@ -5,27 +5,11 @@
 #include "format.h"
 
 
-/* Format */
-int flat_init(FILE * fp, char * arch)
-{
-	return 0;
-}
-
-
-int flat_exit(FILE * fp)
-{
-	return 0;
-}
-
-
-int flat_section(FILE * fp, char * section)
-{
-	return 0;
-}
+/* Flat */
 
 
 FormatPlugin format_plugin = {
-	flat_init,
-	flat_exit,
-	flat_section
+	NULL,
+	NULL,
+	NULL
 };
