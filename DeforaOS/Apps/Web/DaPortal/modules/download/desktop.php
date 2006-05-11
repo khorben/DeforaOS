c891b03e775961e645dbeed32c74b0061920c76b
khorben <>
2006-05-11 17:45:00 +0000

    Improved translation

--- DeforaOS/Apps/Web/DaPortal/modules/download/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/download/desktop.php
@@ -1,6 +1,17 @@
 <?php //modules/download/desktop.php
 
 
+
+//check url
+if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
+	exit(header('Location: ../../index.php'));
+
+
+//lang
+$text['DOWNLOADS'] = 'Downloads';
+global $lang;
+_lang($text);
+
 $title = DOWNLOADS;
 $admin = 1;
 $list = 1;
