36d5c6e2c5973d0a5b8223df5e72d86419e29ee0
khorben <>
2006-05-29 23:20:56 +0000

    Implemented -k

--- DeforaOS/Apps/Unix/src/utils/src/df.c
+++ DeforaOS/Apps/Unix/src/utils/src/df.c
@@ -5,20 +5,26 @@
 #include <sys/statvfs.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+
+
+/* Prefs */
+typedef int Prefs;
+#define PREFS_k 1
 
 
 /* df */
 static int _df_error(char const * message, int ret);
-static int _df_do(char const * file);
-static int _df(int filec, char * filev[])
+static int _df_do(Prefs * prefs, char const * file);
+static int _df(Prefs * prefs, int filec, char * filev[])
 {
 	int ret = 0;
 	int i;
 
-	/* FIXME */
-	printf("%s", "Filesystem 512-blocks       Used  Available Capacity Mounted on\n");
+	printf("%s%s%s", "Filesystem ", *prefs & PREFS_k ? "1024" : " 512",
+			"-blocks       Used  Available Capacity Mounted on\n");
 	for(i = 0; i < filec; i++)
-		ret |= _df_do(filev[i]);
+		ret |= _df_do(prefs, filev[i]);
 	return ret;
 }
 
@@ -29,14 +35,17 @@ static int _df_error(char const * message, int ret)
 	return ret;
 }
 
-static int _df_do(char const * file)
+static int _df_do(Prefs * prefs, char const * file)
 {
 	struct statvfs f;
+	int mod;
 
 	if(statvfs(file, &f) != 0)
 		return _df_error(file, 1);
-	printf("%10s %10d %10d %10d %7d%% %s\n", "", f.f_blocks,
-			f.f_blocks-f.f_bfree, f.f_bavail,
+	mod = f.f_bsize / ((*prefs & PREFS_k) ? 1024 : 512);
+	/* FIXME round up "Use%" result */
+	printf("%11s %10lu %10lu %10lu %7lu%% %s\n", "", f.f_blocks * mod,
+			(f.f_blocks-f.f_bfree) * mod, f.f_bavail * mod,
 			((f.f_blocks-f.f_bfree)*100)/((f.f_blocks-f.f_bfree)
 						      +f.f_bavail), "");
 	return 0;
@@ -55,11 +64,14 @@ static int _usage(void)
 int main(int argc, char * argv[])
 {
 	int o;
+	Prefs prefs;
 
+	memset(&prefs, sizeof(prefs), 0);
 	while((o = getopt(argc, argv, "kPT")) != -1)
 		switch(o)
 		{
 			case 'k':
+				prefs |= PREFS_k;
 				break;
 			case 'P':
 				break;
@@ -68,5 +80,5 @@ int main(int argc, char * argv[])
 			default:
 				return _usage();
 		}
-	return _df(argc - optind, &argv[optind]) == 0 ? 0 : 2;
+	return _df(&prefs, argc - optind, &argv[optind]) == 0 ? 0 : 2;
 }
