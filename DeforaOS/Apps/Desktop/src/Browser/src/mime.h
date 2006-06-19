5afc4a86d0cd7887a768a5e46aa362de5ac9da3d
khorben <>
2006-06-19 13:03:50 +0000

    Supports default actions

--- DeforaOS/Apps/Desktop/src/Browser/src/mime.h
+++ DeforaOS/Apps/Desktop/src/Browser/src/mime.h
@@ -5,6 +5,10 @@
 #ifndef BROWSER_MIME_H
 # define BROWSER_MIME_H
 
+# include <System.h>
+
+# define MIME_CONFIG_FILE ".mime"
+
 
 /* Mime */
 /* types */
@@ -13,12 +17,14 @@ typedef struct _MimeType
 	char * type;
 	char * glob;
 	GdkPixbuf * icon;
+	char * open;
 } MimeType;
 
 typedef struct _Mime
 {
 	MimeType * types;
 	unsigned int types_cnt;
+	Config * config;
 } Mime;
 
 
@@ -28,6 +34,7 @@ void mime_delete(Mime * mime);
 
 /* useful */
 char const * mime_type(Mime * mime, char const * path);
+void mime_open(Mime * mime, char const * path);
 GdkPixbuf * mime_icon(Mime * mime, GtkIconTheme * theme, char const * type);
 
 #endif /* !BROWSER_MIME_H */
