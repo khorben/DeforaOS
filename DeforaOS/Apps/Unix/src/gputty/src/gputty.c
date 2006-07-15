de59fc4ee6741f752ac5eee4fe3d075497dc8c2c
khorben <>
2006-07-15 23:13:44 +0000

    Make about window transient

--- DeforaOS/Apps/Unix/src/gputty/src/gputty.c
+++ DeforaOS/Apps/Unix/src/gputty/src/gputty.c
@@ -311,6 +311,8 @@ static void gputty_on_about(GtkWidget * widget, gpointer data)
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 4);
 	gtk_window_set_title(GTK_WINDOW(window), "About GPuTTY");
+	gtk_window_set_transient(GTK_WINDOW(window), GTK_WINDOW(
+				gputty->window));
 	{
 		GtkWidget * vbox;
 		GtkWidget * hbox;
