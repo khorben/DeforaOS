f18d43bad98378f774a589d3c5c2f02e26217b21
khorben <>
2006-08-04 00:28:21 +0000

    "Downloads" traduction is already in system/lang.php

--- DeforaOS/Apps/Web/DaPortal/modules/download/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/download/desktop.php
@@ -7,11 +7,6 @@ if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
 	exit(header('Location: ../../index.php'));
 
 
-//lang
-$text['DOWNLOADS'] = 'Downloads';
-global $lang;
-_lang($text);
-
 $title = DOWNLOADS;
 $admin = 1;
 $list = 1;
