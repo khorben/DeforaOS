38139456412bbd1fce114b75229fe4e32623b18e
khorben <>
2006-08-06 17:08:23 +0000

    Initial SQLite support (not functional yet)

---
+++ DeforaOS/Apps/Web/DaPortal/system/sql.sqlite.php
@@ -0,0 +1,38 @@
+<?php //system/sql.sqlite.php
+
+
+
+//check url
+if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
+	exit(header('Location: ../index.php'));
+
+define('SQL_TRUE', 1);
+define('SQL_FALSE', 0);
+
+
+function _query($query)
+{
+	global $connection;
+
+	_info($query);
+	if(($res = sqlite_query($connection, $query)) == FALSE)
+		_error(sqlite_error_string(sqlite_last_error($connection)), 0);
+	return $res;
+}
+
+
+function _sql_single($query)
+{
+	if(($res = _query($query)) == FALSE)
+		return FALSE;
+	return sqlite_fetch_single($res);
+}
+
+
+//main
+global $dbname;
+$error = '';
+if(($connection = sqlite_open($dbname, 0666, $error)) == FALSE)
+	_error($error, 0);
+
+?>
