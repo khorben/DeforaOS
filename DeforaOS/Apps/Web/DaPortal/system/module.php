73165012d19e873a6369d8e815ca9953bca04133
khorben <>
2006-09-14 14:13:18 +0000

    Hide errors while including modules

--- DeforaOS/Apps/Web/DaPortal/system/module.php
+++ DeforaOS/Apps/Web/DaPortal/system/module.php
@@ -37,7 +37,7 @@ function _module($module = '', $action = '', $args = FALSE)
 	$module_id = $id;
 	$module_name = $module;
 	if(!include_once('./modules/'.$module_name.'/module.php'))
-		return _error('Could not include module "'.$module_name.'"');
+		return _error('Could not include module "'.$module_name.'"', 0);
 	$function = $module_name.'_'.$action;
 	if(!function_exists($function))
 		return _warning('Unknown action "'.$action.'" for module "'
