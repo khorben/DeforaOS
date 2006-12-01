427fecc188a7a22459b99845f37fdcaa2b61e284
khorben <>
2006-12-01 14:17:24 +0000

    More flexible menu creation allows nested sub-menus

--- DeforaOS/Apps/Web/DaPortal/modules/menu/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/menu/module.php
@@ -7,6 +7,46 @@ if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
 	exit(header('Location: ../../index.php'));
 
 
+function _default_submenu($module, $level, $entry)
+{
+	for($i = 0; $i < $level; $i++)
+		print('	');
+	print('<li>');
+	if(isset($entry['args']))
+		print('<a href="index.php?module='.$module
+				._html_safe_link($entry['args']).'">');
+	print(_html_safe($entry['title']));
+	if(isset($entry['args']))
+		print('</a>');
+	if(isset($entry['actions']) && is_array($entry['actions']))
+	{
+		print("<ul>\n");
+		$actions =& $entry['actions'];
+		$keys = array_keys($actions);
+		foreach($keys as $k)
+		{
+			if(!is_array($actions[$k]))
+			{
+				_default_submenu($module, $level+1, array(
+						'title' => $actions[$k],
+						'args' => '&action='.$k));
+				continue;
+			}
+			$entry = array('title' => $actions[$k]['title']);
+			$entry['args'] = isset($actions[$k]['args'])
+				? $actions[$k]['args'] : '&action='.$k;
+			if(isset($actions[$k]['actions']))
+				$entry['actions'] = $actions[$k]['actions'];
+			_default_submenu($module, $level+1, $entry);
+		}
+		for($i = 0; $i < $level+1; $i++)
+			print('	');
+		print('</ul>');
+	}
+	print("</li>\n");
+}
+
+
 function menu_default()
 {
 	if(($modules = _sql_array('SELECT name FROM daportal_module'
@@ -15,27 +55,13 @@ function menu_default()
 	print('<ul class="menu">'."\n");
 	foreach($modules as $m)
 	{
-		$title = $m['name'];
 		$list = 0;
 		$actions = FALSE;
 		include('./modules/'.$m['name'].'/desktop.php');
 		if(!$list)
 			continue;
-		print("\t".'<li><a href="index.php?module='.$m['name']
-				.'">'.$title.'</a>');
-		if(is_array($actions) && count($actions))
-		{
-			print("<ul>\n");
-			$keys = array_keys($actions);
-			foreach($keys as $k)
-				print("\t\t\t".'<li><a href="index.php?module='
-						.$m['name'].'&amp;action='
-						._html_safe_link($k).'">'
-						._html_safe($actions[$k])
-						.'</a></li>'."\n");
-			print("\t\t</ul>");
-		}
-		print('</li>'."\n");
+		_default_submenu($m['name'], 1, array('title' => $title,
+					'args' => '', 'actions' => $actions));
 	}
 	print('</ul>'."\n");
 }
