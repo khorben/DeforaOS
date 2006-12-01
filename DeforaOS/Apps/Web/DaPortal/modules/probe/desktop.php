4ed51983449c885c03e173343ae0f76db6810673
khorben <>
2006-12-01 14:28:22 +0000

    Listing available hosts directly in the menu

--- DeforaOS/Apps/Web/DaPortal/modules/probe/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/probe/desktop.php
@@ -1,5 +1,4 @@
-<?php
-//modules/project/desktop.php
+<?php //modules/project/desktop.php
 
 
 
@@ -11,6 +10,21 @@ if(!ereg('/index.php$', $_SERVER['PHP_SELF']))
 $title = 'Monitoring';
 $admin = 1;
 $list = 1;
-$actions = array('host_list' => 'Hosts');
+$actions = array('host_list' => array('title' => 'Host list',
+			'args' => '&action=host_list'));
+
+$hosts = _sql_array('SELECT title, host_id AS id'
+		.' FROM daportal_probe_host, daportal_content'
+		." WHERE enabled='1' AND"
+		.' daportal_probe_host.host_id=daportal_content.content_id'
+		.' ORDER BY title ASC');
+if(is_array($hosts))
+{
+	$actions['host_list']['actions'] = array();
+	foreach($hosts as $h)
+		$actions['host_list']['actions'][] = array(
+				'title' => $h['title'],
+				'args' => '&id='.$h['id']);
+}
 
 ?>
