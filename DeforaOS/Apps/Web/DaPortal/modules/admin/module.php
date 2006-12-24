5b55bd2dfdc7f6a9c6b1a6a906603027d7fdd4ad
khorben <>
2006-12-24 16:57:57 +0000

    Check url for no direct call

--- DeforaOS/Apps/Web/DaPortal/modules/admin/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/admin/module.php
@@ -2,6 +2,12 @@
 
 
 
+//check url
+if(strcmp($_SERVER['SCRIPT_NAME'], $_SERVER['PHP_SELF']) != 0
+		|| !ereg('/index.php$', $_SERVER['SCRIPT_NAME']))
+	exit(header('Location: '.dirname($_SERVER['SCRIPT_NAME'])));
+
+
 require_once('./system/user.php');
 
 
