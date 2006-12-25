25af6dd1a063354e4f9c915b1ae0097c95b53102
khorben <>
2006-12-25 20:22:27 +0000

    Update database upon POST only

--- DeforaOS/Apps/Web/DaPortal/modules/project/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/project/module.php
@@ -1234,8 +1234,8 @@ function project_member_insert($args)
 	global $user_id;
 
 	require_once('./system/user.php');
-	if(!_user_admin($user_id))
-		return _error(PERMISSION_DENIED, 1);
+	if(!_user_admin($user_id) || $_SERVER['REQUEST_METHOD'] != 'POST')
+		return _error(PERMISSION_DENIED);
 	if(($id = _sql_single('SELECT user_id FROM daportal_project_user'
 			." WHERE project_id='".$args['project_id']."'"
 			." AND user_id='".$args['id']."'")) == $args['id'])
