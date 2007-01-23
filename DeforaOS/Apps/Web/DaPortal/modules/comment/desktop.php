daef84a32576bb916d7827b366cc71d67c909c30
khorben <>
2007-01-23 20:54:31 +0000

    Extra args are not needed

--- DeforaOS/Apps/Web/DaPortal/modules/comment/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/comment/desktop.php
@@ -5,8 +5,6 @@ $title = 'Comments';
 $icon = 'comment.png';
 $admin = 1;
 $list = 0;
-global $user_id;
-$user = array(array('icon' => 'comment.png', 'name' => $title,
-			'args' => '&user_id='.$user_id));
+$user = array(array('icon' => 'comment.png', 'name' => $title));
 
 ?>
--- DeforaOS/Apps/Web/DaPortal/modules/news/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/news/desktop.php
@@ -8,12 +8,9 @@ $admin = 1;
 $list = 1;
 global $user_id;
 if($user_id)
-{
 	$actions = array('submit' => SUBMIT);
-	$user = array(array('icon' => 'news.png', 'name' => $title,
-				'action' => 'list',
-				'args' => '&user_id='.$user_id));
-}
+$user = array(array('icon' => 'news.png', 'name' => $title,
+			'action' => 'list'));
 
 
 ?>
