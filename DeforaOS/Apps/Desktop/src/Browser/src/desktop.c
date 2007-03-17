f6a029390f2e8f20babb79f77edc626c8e8ffc7f
khorben <>
2007-03-17 23:10:08 +0000

    Gtk+ < 2.6 compatibility fix

--- DeforaOS/Apps/Desktop/src/Browser/src/desktop.c
+++ DeforaOS/Apps/Desktop/src/Browser/src/desktop.c
@@ -372,12 +372,21 @@ Desktop * desktop_new(void)
 	char * home;
 	struct stat st;
 	DesktopIcon * desktopicon;
-	char * file[] = { "gnome-fs-regular", GTK_STOCK_FILE,
+	char * file[] = { "gnome-fs-regular",
+#if GTK_CHECK_VERSION(2, 6, 0)
+		GTK_STOCK_FILE,
+#endif
 		GTK_STOCK_MISSING_IMAGE, NULL };
-	char * folder[] = { "gnome-fs-directory", GTK_STOCK_DIRECTORY,
+	char * folder[] = { "gnome-fs-directory",
+#if GTK_CHECK_VERSION(2, 6, 0)
+		GTK_STOCK_DIRECTORY,
+#endif
 		GTK_STOCK_MISSING_IMAGE, NULL };
 	char * executable[] = { "gnome-fs-executable", "gnome-fs-regular",
-		GTK_STOCK_FILE, GTK_STOCK_MISSING_IMAGE, NULL };
+#if GTK_CHECK_VERSION(2, 6, 0)
+		GTK_STOCK_FILE,
+#endif
+		GTK_STOCK_MISSING_IMAGE, NULL };
 	char ** p;
 
 	if((desktop = malloc(sizeof(*desktop))) == NULL)
