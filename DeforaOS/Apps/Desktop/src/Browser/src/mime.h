5165b2f9f21ae4247d4b8f994d42d97bc74a35b4
khorben <>
2007-05-16 22:33:22 +0000

    Updated copyright line

--- DeforaOS/Apps/Desktop/src/Browser/src/mime.h
+++ DeforaOS/Apps/Desktop/src/Browser/src/mime.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2006 The DeforaOS Project */
+/* Copyright (c) 2007 The DeforaOS Project */
 /* This file is part of Browser */
 /* Browser is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
