a685dd56d1f02cfa66b5f0bac0f41ea6fdd3d25b
khorben <>
2007-06-13 17:58:05 +0000

    Licensed under the GPL version 2

--- DeforaOS/Apps/Desktop/src/Editor/src/main.c
+++ DeforaOS/Apps/Desktop/src/Editor/src/main.c
@@ -1,5 +1,17 @@
 /* $Id$ */
-/* Copyright (c) 2006 The DeforaOS Project */
+/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Desktop Editor */
+/* Editor is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * Editor is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Editor; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA  02111-1307  USA */
 
 
 
