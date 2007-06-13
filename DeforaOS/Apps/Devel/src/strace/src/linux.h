a9d92b27cfe69276f8d70e7b7798557df2bb4a87
khorben <>
2007-06-13 18:49:05 +0000

    Properly licensed under the terms of the DeforaOS Project

--- DeforaOS/Apps/Devel/src/strace/src/linux.h
+++ DeforaOS/Apps/Devel/src/strace/src/linux.h
@@ -1,9 +1,23 @@
-/* linux.h */
+/* $Id$ */
+/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Devel strace */
+/* strace is not free software; you can redistribute it and/or modify it under
+ * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported as published by the Creative Commons organization.
+ *
+ * strace is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with strace; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
 
 
 
-#ifndef _LINUX_H
-# define _LINUX_H
+#ifndef STRACE_LINUX_H
+# define STRACE_LINUX_H
 
 # include <linux/user.h>
 # include <asm/unistd.h>
@@ -11,4 +25,4 @@
 
 extern char * syscall[__NR_getpid];
 
-#endif /* !_LINUX_H */
+#endif /* !STRACE_LINUX_H */
