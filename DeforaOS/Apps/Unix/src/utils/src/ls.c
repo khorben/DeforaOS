76d32a390110628bdb915aab687194f08a2bf907
khorben <>
2007-06-13 19:03:50 +0000

    Properly licensed as part of the DeforaOS Project

--- DeforaOS/Apps/Unix/src/utils/src/ls.c
+++ DeforaOS/Apps/Unix/src/utils/src/ls.c
@@ -1,5 +1,18 @@
 /* $Id$ */
-/* Copyright (c) 2007 The DeforaOS Project */
+/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Unix utils */
+/* utils is not free software; you can redistribute it and/or modify it under
+ * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported as published by the Creative Commons organization.
+ *
+ * utils is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with utils; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
 /* TODO: use a dynamic array and qsort() instead of lists */
 
 
