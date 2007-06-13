58496264eef52608f46243b38a357ac78419c6ad
khorben <>
2007-06-13 21:01:58 +0000

    Licensing under the GPL version 2

--- DeforaOS/Apps/Web/DaPortal/engine.php
+++ DeforaOS/Apps/Web/DaPortal/engine.php
@@ -1,11 +1,10 @@
-<?php //engine.php
-//Copyright (c) 2004, 2005, 2006, 2007 The DeforaOS Project
+<?php //$Id$
+//Copyright (c) 2004, 2005, 2006, 2007 Pierre Pronchery <khorben@defora.org>
 //This file is part of DaPortal
 //
 //DaPortal is free software; you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation; either version 2 of the License, or
-//(at your option) any later version.
+//it under the terms of the GNU General Public License version 2 as
+//published by the Free Software Foundation.
 //
 //DaPortal is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
