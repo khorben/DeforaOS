3ab1128767738abfdcd187a8e32fcadce200bab0
khorben <>
2007-06-14 00:05:25 +0000

    Licensed under the terms of the DeforaOS Project license

--- DeforaOS/System/src/Init/src/init.c
+++ DeforaOS/System/src/Init/src/init.c
@@ -1,5 +1,18 @@
 /* $Id$ */
-/* Copyright (c) 2007 The DeforaOS Project */
+/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS System Init */
+/* Init is not free software; you can redistribute it and/or modify it
+ * under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike
+ * 3.0 Unported as published by the Creative Commons organization.
+ *
+ * Init is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with Init; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
 
 
 
