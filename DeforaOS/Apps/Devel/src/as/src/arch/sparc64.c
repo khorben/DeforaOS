487f4554b1140c9e0cea187f5704f3e0c1cc0ca3
khorben <>
2007-08-07 02:09:50 +0000

    Updated sparc64

--- DeforaOS/Apps/Devel/src/as/src/arch/sparc64.c
+++ DeforaOS/Apps/Devel/src/as/src/arch/sparc64.c
@@ -20,14 +20,29 @@
 #include "arch.h"
 
 
+/* types */
+#define REG(name, size, id) AO_ ## name = ((id << 2) | AO_REG), \
+				AO_ ## name ## _ = ((id << 10) | AO_REG_), \
+				AO_ ## name ## __ = ((id << 18) | AO_REG__),
+enum
+{
+#include "common.reg"
+#include "sparc64.reg"
+};
+
+
 /* variables */
+#undef REG
+#define REG(name, size, id) { "" # name, size, id },
 ArchRegister arch_sparc64_regs[] =
 {
+#include "sparc64.reg"
 	{ NULL,	0,	0 }
 };
 
 ArchInstruction arch_sparc64_set[] =
 {
+#include "sparc64.ins"
 	{ NULL,	0x0,	0, 0, 0, 0 }
 };
 
