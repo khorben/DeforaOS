2c01816faa3bd6cc1d014f154a6ed2fc7d4ba811
khorben <>
2007-08-17 16:51:42 +0000

    Releasing Editor 0.0.1

--- DeforaOS/Apps/Desktop/src/Editor/Makefile
+++ DeforaOS/Apps/Desktop/src/Editor/Makefile
@@ -1,5 +1,5 @@
 PACKAGE	= Editor
-VERSION	= 0.0.0
+VERSION	= 0.0.1
 SUBDIRS	= src
 RM	= rm -f
 LN	= ln -sf
--- DeforaOS/Apps/Desktop/src/Editor/config.h
+++ DeforaOS/Apps/Desktop/src/Editor/config.h
@@ -1,5 +1,5 @@
 #define PACKAGE "Editor"
-#define VERSION "0.0.0"
+#define VERSION "0.0.1"
 
 #ifndef PREFIX
 # define PREFIX "/usr/local"
