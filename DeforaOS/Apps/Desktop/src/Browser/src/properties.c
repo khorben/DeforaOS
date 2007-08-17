2358310a1fca805bfe3a43585e3b2f6002b87f21
khorben <>
2007-08-17 16:18:24 +0000

    Had to re-license under the DeforaOS Project License to avoid conflict

--- DeforaOS/Apps/Desktop/src/Browser/src/properties.c
+++ DeforaOS/Apps/Desktop/src/Browser/src/properties.c
@@ -1,17 +1,18 @@
 /* $Id$ */
 /* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Desktop Browser */
-/* Browser is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 2 as published by the Free
- * Software Foundation.
+/* Browser is not free software; you can redistribute it and/or modify it
+ * under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike
+ * 3.0 Unported as published by the Creative Commons organization.
  *
  * Browser is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * A PARTICULAR PURPOSE. See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * Browser; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA  02111-1307  USA */
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with Browser; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
 /* FIXME:
  * - add dates */
 
