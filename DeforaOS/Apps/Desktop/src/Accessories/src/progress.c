182f4105f9d8a7765c05b3dd5a57b08029089f4e
khorben <>
2007-09-15 12:25:42 +0000

    Speed estimation is implemented

--- DeforaOS/Apps/Desktop/src/Accessories/src/progress.c
+++ DeforaOS/Apps/Desktop/src/Accessories/src/progress.c
@@ -14,7 +14,6 @@
  * Accessories; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA  02111-1307  USA */
 /* TODO:
- * - speed estimation
  * - implement -z */
 
 
