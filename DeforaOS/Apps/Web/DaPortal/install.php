df20ff969e57f9ea44867ec4ce51bb771a558a3b
khorben <>
2007-10-31 18:29:24 +0000

    Finally working again (tested only with SQLite)

--- DeforaOS/Apps/Web/DaPortal/install.php
+++ DeforaOS/Apps/Web/DaPortal/install.php
@@ -14,18 +14,23 @@
 //You should have received a copy of the GNU General Public License
 //along with DaPortal; if not, write to the Free Software
 //Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//TODO:
+//- test thoroughly on all supported SQL backends
 
 
 
 //check url
-if(eregi('install.php', $_SERVER['REQUEST_URI']))
-{
-	header('Location: index.php');
-	exit(1);
-}
+if(!ereg('/install.php$', $_SERVER['SCRIPT_NAME']))
+	exit(header('Location: index.php'));
 
-exit(0); //FIXME DaPortal has been restarted from scratch
-require_once('system/raw.php');
+
+//variables
+$dbtype = FALSE;
+$dbhost = FALSE;
+$dbport = FALSE;
+$dbname = FALSE;
+$dbuser = FALSE;
+$dbpassword = FALSE;
 
 
 //installing functions
@@ -33,96 +38,62 @@ require_once('system/raw.php');
 //resets and creates default tables
 //PRE
 //POST
-function install_database()
+function install_database($dbtype, $dbhost, $dbport, $dbname, $dbuser,
+		$dbpassword)
 {
-	sql_table_drop('daportal_sessions');
-	sql_table_drop('daportal_users');
-	sql_table_drop('daportal_layouts');
-	sql_table_drop('daportal_contents');
-	sql_table_drop('daportal_modules');
-	sql_sequence_drop('daportal_users_userid_seq');
-	sql_sequence_drop('daportal_modules_moduleid_seq');
-	sql_sequence_drop('daportal_contents_contentid_seq');
+	global $connection;
 
-	sql_sequence_create('daportal_users_userid_seq');
-	sql_sequence_create('daportal_modules_moduleid_seq');
-	sql_sequence_create('daportal_contents_contentid_seq');
-	//FIXME move users and sessions to module users,
-	//and set up login procedure depending on module users enabled
-	sql_table_create('daportal_users', "(
-	userid integer DEFAULT nextval('daportal_users_userid_seq'),
-	username varchar(9) NOT NULL UNIQUE,
-	password varchar(32) NOT NULL,
-	moderator bool NOT NULL DEFAULT '0',
-	administrator bool NOT NULL DEFAULT '0',
-	theme varchar(9) NOT NULL DEFAULT '',
-	email varchar(60) NOT NULL DEFAULT '',
-	homepage varchar(60) NOT NULL DEFAULT '',
-	PRIMARY KEY (userid)
-)");
-	sql_table_create('daportal_sessions', "(
-	sessionid varchar(32),
-	userid integer,
-	ip varchar(15) NOT NULL,
-	expires date NOT NULL,
-	FOREIGN KEY (userid) REFERENCES daportal_users (userid)
-)");
-	sql_table_create('daportal_modules', "(
-	moduleid integer DEFAULT nextval('daportal_modules_moduleid_seq'),
-	modulename varchar(9) NOT NULL UNIQUE,
-	enable bool NOT NULL DEFAULT '1',
-	PRIMARY KEY (moduleid)
-)");
-	sql_table_create('daportal_contents', "(
-	contentid integer DEFAULT nextval('daportal_contents_contentid_seq'),
-	moduleid integer,
-	title varchar(80) NOT NULL DEFAULT '',
-	content text NOT NULL,
-	enable bool NOT NULL DEFAULT '0',
-	PRIMARY KEY (contentid),
-	FOREIGN KEY (moduleid) REFERENCES daportal_modules (moduleid)
-)");
-
-	sql_table_create('daportal_layouts', "(
-	moduleid integer,
-	action varchar(9) NOT NULL DEFAULT '',
-	top varchar(9) NOT NULL DEFAULT '',
-	bottom varchar(9) NOT NULL DEFAULT '',
-	FOREIGN KEY (moduleid) REFERENCES daportal_modules (moduleid)
-)");
-
-	sql_query("insert into daportal_users (username, password, administrator) values ('admin', '5f4dcc3b5aa765d61d8327deb882cf99', '1');");
-
-	require_once('system/module.php');
-	module_install('admin');
-	module_install('user');
-	module_install('pages');
-	module_install('news');
+	//quirks
+	switch($dbtype)
+	{
+		case 'mysql':
+			break;
+		case 'pgsql':
+			break;
+		case 'sqlite':
+			//XXX security issue but the user is trusted (127.0.0.1)
+			if(!file_exists($dbname))
+				if(touch($dbname) != TRUE)
+					return 1;
+			break;
+		default: //unknown database
+			return 1;
+	}
+	$connection = FALSE;
+	require_once('./system/sql.php');
+	if($connection == FALSE)
+		return 1;
+	if(($sql = file_get_contents('install/'.$dbtype.'.sql')) == FALSE)
+		return 1;
+	if($dbtype == 'sqlite') //XXX one more quirk
+		sqlite_query($connection, $sql); //XXX can't check errors?
+	else if(_sql_query($sql) == FALSE)
+		return 1;
 	return 0;
 }
 
-function install_config($dbtype, $dbhost, $dbport, $dbname, $dbuser, $dbpassword)
+function install_config($dbtype, $dbhost, $dbport, $dbname, $dbuser,
+		$dbpassword, $template, $theme)
 {
 	if(($fp = fopen('config.php', 'w')) == FALSE)
 		return 1;
-	fwrite($fp, "<?php
-
+	fwrite($fp, "<?php //config.php
 
 
 //check url
-if(eregi(\"config.php\", \$_SERVER[\"REQUEST_URI\"]))
-{
-	header(\"Location: index.php\");
-	exit(0);
-}
+if(!ereg('/index.php$', \$_SERVER['SCRIPT_NAME']))
+	exit(header('Location: index.php'));
 
-\$dbtype = \"$dbtype\";
-\$dbhost = \"$dbhost\";
-\$dbport = \"$dbport\";
-\$dbname = \"$dbname\";
-\$dbuser = \"$dbuser\";
-\$dbpassword = \"$dbpassword\";
 
+\$template = '$template';
+\$theme = '$theme';
+//\$debug = 1;		//force debugging on
+\$dbtype = '$dbtype';	//'mysql', 'pgsql' or 'sqlite'
+\$dbhostname = '$dbhost';	//defaults to local connections
+\$dbname = '$dbname';
+\$dbuser = '$dbuser';
+\$dbpassword = '$dbpassword';
+\$friendlylinks = 0;	//force \"friendly links\" generation
 
 ?>\n");
 	fclose($fp);
@@ -138,13 +109,13 @@ function install_index()
 
 
 //check url
-if(!eregi('index.php', \$_SERVER['REQUEST_URI']))
+if(!ereg('/index.php$', \$_SERVER['SCRIPT_NAME']))
 {
 	header('Location: index.php');
 	exit(0);
 }
 
-include('engine.php');
+include('./engine.php');
 
 
 ?>\n");
@@ -156,14 +127,19 @@ include('engine.php');
 //PRE
 //POST	1	error
 //	0	success
-function install_process($dbtype, $dbhost, $dbport, $dbname, $dbuser, $dbpassword)
+function install_process($dbtype, $dbhost, $dbport, $dbname, $dbuser,
+		$dbpassword, $template, $theme)
 {
-	if(install_database() != 0
-			|| install_config($dbtype, $dbhost, $dbport,
-					$dbname, $dbuser, $dbpassword) != 0
+	if(install_database($dbtype, $dbhost, $dbport, $dbname, $dbuser,
+				$dbpassword) != 0
+			|| install_config($dbtype, $dbhost, $dbport, $dbname,
+				$dbuser, $dbpassword, $template, $theme) != 0
 			|| install_index() != 0)
 		return 1;
-	header('Location: index.php');
+	session_start();
+	$_SESSION['user_id'] = 1;
+	$_SESSION['user_name'] = 'admin';
+	header('Location: index.php?module=user&action=admin&id=1');
 	return 0;
 }
 
@@ -174,9 +150,9 @@ function install_process($dbtype, $dbhost, $dbport, $dbname, $dbuser, $dbpasswor
 //POST	1	success
 function install_get()
 {
-	if(raw_include('html/xhtml.html') != 0)
+	if(readfile('html/doctype.html') == FALSE)
 		print("<html>\n");
-	if(raw_include('html/install.html') != 0)
+	if(readfile('html/install.html') == FALSE)
 		print("\t<head>
 \t\t<title>DaPortal configuration</title>
 \t</head>
@@ -198,50 +174,19 @@ function install_get()
 //	1	error
 function install_post()
 {
-	$dbtype = $_POST["dbtype"];
-	$dbhost = $_POST["dbhost"];
-	$dbport = $_POST["dbport"];
-	$dbuser = $_POST["dbuser"];
-	$dbpassword = $_POST["dbpassword"];
-	$dbname = $_POST["dbname"];
+	global $connection, $dbtype, $dbhost, $dbport, $dbname, $dbuser,
+	       $dbpassword;
 
-	require_once("system/sql.php");
-	//FIXME postgresql only
-	//FIXME check input (in sql_connect?)
-	if(sql_connect($dbhost, $dbport, $dbname, $dbuser, $dbpassword) != 0)
-	{
-		if(raw_include("html/xhtml.html") != 0)
-			print("<html>\n");
-		print("\t<head>
-\t\t<title>DaPortal processing configuration</title>
-\t</head>
-\t<body>
-\t<h3>Connection to the database failed</h3>
-\t<p>
-\t\t<b>Summary of your settings:</b><br/>
-\t\t<b>Server type</b>: ");
-		switch($dbtype)
-		{
-			case "pgsql":
-				print("PostgreSQL<br/>\n");
-				break;
-			default:
-				print("UNKNOWN<br/>\n");
-				break;
-		}
-		print("\t\t<b>Hostname</b>: $dbhost<br/>
-\t\t<b>Port</b>: $dbport<br/>
-\t\t<b>Username</b>: $dbuser<br/>
-\t\t<b>Password</b>: (hidden)<br/>
-\t\t<b>Database</b>: $dbname<br/>
-\t</p>
-\t</body>
-</html>\n");
-		return 1;
-	}
+	require_once('./system/debug.php');
+	if(!get_magic_quotes_gpc())
+		exit(_error('Magic quotes must be enabled'));
+	$vars = array('dbtype', 'dbhost', 'dbport', 'dbname', 'dbuser',
+			'dbpassword', 'template', 'theme');
+	foreach($vars as $v)
+		$$v = isset($_POST[$v]) ? stripslashes($_POST[$v]) : FALSE;
 	if(!is_writable('.'))
 	{
-		if(raw_include('html/xhtml.html') != 0)
+		if(readfile('html/doctype.html') == FALSE)
 			print("<html>\n");
 		print("\t<head>
 \t\t<title>DaPortal processing configuration</title>
@@ -254,7 +199,9 @@ function install_post()
 </html>\n");
 		return 1;
 	}
-	return install_process($dbtype, $dbhost, $dbport, $dbname, $dbuser, $dbpassword);
+	$_SERVER['SCRIPT_NAME'] = '/index.php'; //XXX
+	return install_process($dbtype, $dbhost, $dbport, $dbname, $dbuser,
+			$dbpassword, $template, $theme);
 }
 
 
@@ -263,16 +210,17 @@ function install_post()
 //POST	exits with error
 function install_invalid()
 {
-	raw_require("html/xhtml.html");
-	raw_require("html/invalid.html");
+	readfile('html/doctype.html');
+	readfile('html/invalid.html');
 	exit(1);
 }
 
 
-//process requests
-if($_SERVER["REMOTE_ADDR"] != "127.0.0.1")
+//main
+$html = 1;
+if($_SERVER['REMOTE_ADDR'] != '127.0.0.1')
 {
-	if(raw_include("html/xhtml.html") != 0)
+	if(readfile('html/doctype.html') != 0)
 		print("<html>\n");
 	print("\t<head>
 \t\t<title>Server maintainance</title>
@@ -287,11 +235,9 @@ if($_SERVER["REMOTE_ADDR"] != "127.0.0.1")
 \t\t</p>\n");
 	return 1;
 }
-global $administrator;
-$administrator = 1;
-if($_SERVER["REQUEST_METHOD"] == "POST")
+if($_SERVER['REQUEST_METHOD'] == 'POST')
 	return install_post();
-if($_SERVER["REQUEST_METHOD"] != "GET")
+if($_SERVER['REQUEST_METHOD'] != 'GET')
 	return install_invalid();
 return install_get();
 
