06c7f169e29b62a2b7b86d8fbcaf6fdec0c3e206
khorben <>
2007-11-01 02:16:19 +0000

    Updated some French translations

--- DeforaOS/Apps/Web/DaPortal/modules/comment/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/comment/desktop.php
@@ -20,6 +20,9 @@ $title = 'Comments';
 $icon = 'comment.png';
 $admin = 1;
 $list = 0;
-$user = array(array('icon' => 'comment.png', 'name' => $title));
+global $lang;
+if($lang == 'fr')
+	$title = 'Commentaires';
+$user = array(array('icon' => $icon, 'name' => $title));
 
 ?>
