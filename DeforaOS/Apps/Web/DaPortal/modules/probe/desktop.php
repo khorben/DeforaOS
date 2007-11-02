67fb6a9a1ab0c0689f1a7433cc19d69200cdfdf7
khorben <>
2007-11-02 00:02:04 +0000

    Paving the way for better translations

--- DeforaOS/Apps/Web/DaPortal/modules/probe/desktop.php
+++ DeforaOS/Apps/Web/DaPortal/modules/probe/desktop.php
@@ -29,23 +29,48 @@ $list = 1;
 $actions = array('graph_list' => array('title' => 'Graphs'),
 		'host_list' => array('title' => 'Hosts',
 			'args' => 'action=host_list'));
+global $lang;
+if($lang == 'fr')
+{
+	$actions['graph_list']['title'] = 'Graphes';
+	$actions['host_list']['title'] = 'Machines';
+}
+$text['UPTIME'] = 'Uptime';
+$text['LOAD_AVERAGE'] = 'Load average';
+$text['LOGGED_USERS'] = 'Logged users';
+$text['MEMORY_USAGE'] = 'Memory usage';
+$text['NETWORK_TRAFFIC'] = 'Network traffic';
+$text['PROCESS_COUNT'] = 'Process count';
+$text['SWAP_USAGE'] = 'Swap usage';
+$text['VOLUME_USAGE'] = 'Volume usage';
+if($lang == 'fr')
+{
+	$text['LOAD_AVERAGE'] = 'Charge moyenne';
+	$text['LOGGED_USERS'] = 'Utilisateurs connectés';
+	$text['MEMORY_USAGE'] = 'Utilisation mémoire';
+	$text['NETWORK_TRAFFIC'] = 'Activité réseau';
+	$text['PROCESS_COUNT'] = 'Nombre de processus';
+	$text['SWAP_USAGE'] = 'Utilisation échange';
+	$text['VOLUME_USAGE'] = 'Utilisation des volumes';
+}
+_lang($text);
 
 $actions['graph_list']['actions'] = array();
-$actions['graph_list']['actions'][] = array('title' => 'Uptime',
+$actions['graph_list']['actions'][] = array('title' => UPTIME,
 		'args' => 'type=uptime');
-$actions['graph_list']['actions'][] = array('title' => 'Load average',
+$actions['graph_list']['actions'][] = array('title' => LOAD_AVERAGE,
 		'args' => 'type=load');
-$actions['graph_list']['actions'][] = array('title' => 'Memory usage',
+$actions['graph_list']['actions'][] = array('title' => MEMORY_USAGE,
 		'args' => 'type=ram');
-$actions['graph_list']['actions'][] = array('title' => 'Swap usage',
+$actions['graph_list']['actions'][] = array('title' => SWAP_USAGE,
 		'args' => 'type=swap');
-$actions['graph_list']['actions'][] = array('title' => 'Logged users',
+$actions['graph_list']['actions'][] = array('title' => LOGGED_USERS,
 		'args' => 'type=users');
-$actions['graph_list']['actions'][] = array('title' => 'Process count',
+$actions['graph_list']['actions'][] = array('title' => PROCESS_COUNT,
 		'args' => 'type=procs');
-$actions['graph_list']['actions'][] = array('title' => 'Network traffic',
+$actions['graph_list']['actions'][] = array('title' => NETWORK_TRAFFIC,
 		'args' => 'type=iface');
-$actions['graph_list']['actions'][] = array('title' => 'Volume usage',
+$actions['graph_list']['actions'][] = array('title' => VOLUME_USAGE,
 		'args' => 'type=vol');
 
 $hosts = _sql_array('SELECT title, content_id AS id'
