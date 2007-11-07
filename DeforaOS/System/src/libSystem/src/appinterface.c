37077762a46e2754837e908057e2e8262f7d84fc
khorben <>
2007-11-07 12:08:24 +0000

    Added lseek() to VFS

--- DeforaOS/System/src/libSystem/src/appinterface.c
+++ DeforaOS/System/src/libSystem/src/appinterface.c
@@ -310,10 +310,10 @@ static int _new_vfs(AppInterface * ai)
 	/* ret |= _new_append(ai, AICT_UINT32, "lchown", 3, AICT_STRING,
 			AICT_UINT32, AICT_UINT32);
 	ret |= _new_append(ai, AICT_UINT32, "link", 2, AICT_STRING,
-			AICT_STRING);
-	ret |= _new_append(ai, AICT_UINT32, "lseek", 3, AICT_UINT32,
-			AICT_UINT32, AICT_UINT32);
-	ret |= _new_append(ai, AICT_UINT32, "lstat", 2, AICT_STRING,
+			AICT_STRING); */
+	ret |= _new_append(ai, AICT_INT32, "lseek", 3, AICT_INT32, AICT_INT32,
+			AICT_INT32);
+/*	ret |= _new_append(ai, AICT_UINT32, "lstat", 2, AICT_STRING,
 			AICT_BUFFER | AICD_OUT);
 	ret |= _new_append(ai, AICT_UINT32, "mkdir", 2, AICT_STRING,
 			AICT_UINT32); */
