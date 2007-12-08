59697011721add34d7e4f622f28c88fb4cf26e88
khorben <>
2007-12-08 13:27:12 +0000

    Updated copyright and license

--- DeforaOS/Apps/Desktop/src/Browser/src/view.c
+++ DeforaOS/Apps/Desktop/src/Browser/src/view.c
@@ -1,18 +1,20 @@
 /* $Id$ */
-/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+static char const _copyright[] =
+"Copyright (c) 2007 Pierre Pronchery <khorben@defora.org>";
 /* This file is part of DeforaOS Desktop Browser */
-/* Browser is not free software; you can redistribute it and/or modify it
- * under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike
- * 3.0 Unported as published by the Creative Commons organization.
- *
- * Browser is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the Creative Commons Attribution-NonCommercial-
- * ShareAlike 3.0 Unported license for more details.
- *
- * You should have received a copy of the Creative Commons Attribution-
- * NonCommercial-ShareAlike 3.0 along with Browser; if not, browse to
- * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+static char const _license[] =
+"Browser is not free software; you can redistribute it and/or modify it\n"
+"under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike\n"
+"3.0 Unported as published by the Creative Commons organization.\n"
+"\n"
+"Browser is distributed in the hope that it will be useful, but WITHOUT ANY\n"
+"WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS\n"
+"FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution-\n"
+"NonCommercial-ShareAlike 3.0 Unported license for more details.\n"
+"\n"
+"You should have received a copy of the Creative Commons Attribution-\n"
+"NonCommercial-ShareAlike 3.0 along with Browser; if not, browse to\n"
+"http://creativecommons.org/licenses/by-nc-sa/3.0/";
 
 
 
@@ -52,8 +54,6 @@ static char const * _authors[] =
 	"Pierre Pronchery <khorben@defora.org>",
 	NULL
 };
-static char const _copyright[] = "Copyright (c) 2007 Pierre Pronchery";
-static char const _license[] = "GPL version 2"; /* FIXME complete */
 
 
 /* View */
