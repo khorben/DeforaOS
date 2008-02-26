28b48cdca0a24cf6a104966520ada04de01d74af
khorben <>
2008-02-26 09:39:51 +0000

    Added NNTP plug-in

---
+++ DeforaOS/Apps/Desktop/src/Mailer/src/account/nntp.c
@@ -0,0 +1,47 @@
+/* $Id$ */
+/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Desktop Mailer */
+/* Mailer is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * Mailer is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Mailer; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA  02111-1307  USA */
+
+
+
+#include <stdlib.h>
+#include "account.h"
+
+
+/* variables */
+char const nntp_type[] = "NNTP";
+char const nntp_name[] = "Newsgroups";
+
+AccountConfig nntp_config[] =
+{
+	{ "username",	"Username",		ACT_STRING,	NULL	},
+	{ "password",	"Password",		ACT_PASSWORD,	NULL	},
+	{ "hostname",	"Server hostname",	ACT_STRING,	NULL	},
+	{ "port",	"Server port",		ACT_UINT16,	119	},
+	{ "ssl",	"Use SSL",		ACT_BOOLEAN,	0	},
+	{ NULL,		NULL,			ACT_NONE,	NULL	}
+};
+
+
+/* functions */
+
+
+AccountPlugin account_plugin =
+{
+	nntp_type,
+	nntp_name,
+	nntp_config,
+	NULL,
+	NULL
+};
