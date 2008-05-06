7930d9e37aaeab44f0b2f8a7466f80b8c28d490c
khorben <>
2008-05-06 17:29:06 +0000

    Working on floating-point instructions like fcmov*

--- DeforaOS/Apps/Devel/src/as/src/test/i386.asm
+++ DeforaOS/Apps/Devel/src/as/src/test/i386.asm
@@ -79,9 +79,61 @@
 	fsin
 	fcos
 	/* 0xda */
+	/* 0xdac0 */
+	fcmovb	%st0, %st0
+	fcmovb	%st0, %st1
+	fcmovb	%st0, %st2
+	fcmovb	%st0, %st3
+	fcmovb	%st0, %st4
+	fcmovb	%st0, %st5
+	fcmovb	%st0, %st6
+	fcmovb	%st0, %st7
+	fcmove	%st0, %st0
+	fcmove	%st0, %st1
+	fcmove	%st0, %st2
+	fcmove	%st0, %st3
+	fcmove	%st0, %st4
+	fcmove	%st0, %st5
+	fcmove	%st0, %st6
+	fcmove	%st0, %st7
+	/* 0xdad0 */
+	fcmovbe	%st0, %st0
+	fcmovbe	%st0, %st1
+	fcmovbe	%st0, %st2
+	fcmovbe	%st0, %st3
+	fcmovbe	%st0, %st4
+	fcmovbe	%st0, %st5
+	fcmovbe	%st0, %st6
+	fcmovu	%st0, %st7
+	fcmovu	%st0, %st0
+	fcmovu	%st0, %st1
+	fcmovu	%st0, %st2
+	fcmovu	%st0, %st3
+	fcmovu	%st0, %st4
+	fcmovu	%st0, %st5
+	fcmovu	%st0, %st6
+	fcmovu	%st0, %st7
 	fucompp
 	/* 0xdb */
 	fnclex
+	/* 0xdbe8 */
+	fucomi	%st0, %st0
+	fucomi	%st0, %st1
+	fucomi	%st0, %st2
+	fucomi	%st0, %st3
+	fucomi	%st0, %st4
+	fucomi	%st0, %st5
+	fucomi	%st0, %st6
+	fucomi	%st0, %st7
+	/* 0xdbf0 */
+	fcomi	%st0, %st0
+	fcomi	%st0, %st1
+	fcomi	%st0, %st2
+	fcomi	%st0, %st3
+	fcomi	%st0, %st4
+	fcomi	%st0, %st5
+	fcomi	%st0, %st6
+	fcomi	%st0, %st7
 	/* 0xdc */
 	fdiv	%st0, %st0, %st0
 	fdiv	%st1, %st0, %st1
@@ -115,6 +167,24 @@
 	fdivp	%st5, %st0, %st5
 	fdivp	%st6, %st0, %st6
 	fdivp	%st7, %st0, %st7
+	/* 0xdfe8 */
+	fucomip	%st0, %st0
+	fucomip	%st0, %st1
+	fucomip	%st0, %st2
+	fucomip	%st0, %st3
+	fucomip	%st0, %st4
+	fucomip	%st0, %st5
+	fucomip	%st0, %st6
+	fucomip	%st0, %st7
+	/* 0xdff0 */
+	fcomip	%st0, %st0
+	fcomip	%st0, %st1
+	fcomip	%st0, %st2
+	fcomip	%st0, %st3
+	fcomip	%st0, %st4
+	fcomip	%st0, %st5
+	fcomip	%st0, %st6
+	fcomip	%st0, %st7
 	/* 0xf0 */
 	cmc
 	clc
