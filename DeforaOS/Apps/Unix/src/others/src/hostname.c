442bd765cbf85c65cef58b8683373b1fb87b2947
khorben <>
2008-09-06 16:42:14 +0000

    Initial release

---
+++ DeforaOS/Apps/Unix/src/others/src/hostname.c
@@ -0,0 +1,57 @@
+/* $Id$ */
+/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Unix others */
+/* others is not free software; you can redistribute it and/or modify it under
+ * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported as published by the Creative Commons organization.
+ *
+ * others is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with others; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+
+
+
+#include <unistd.h>
+#include <stdio.h>
+
+
+/* hostname */
+static int _hostname(void)
+{
+	char buf[128];
+
+	if(gethostname(buf, sizeof(buf)) != 0)
+		return 1;
+	printf("%s\n", buf);
+	return 0;
+}
+
+
+/* usage */
+static int _usage(void)
+{
+	fputs("Usage: hostname\n", stderr);
+	return 1;
+}
+
+
+/* main */
+int main(int argc, char * argv[])
+{
+	int o;
+
+	while((o = getopt(argc, argv, "")) != -1)
+		switch(o)
+		{
+			default:
+				return _usage();
+		}
+	if(optind != argc)
+		return _usage();
+	return _hostname() == 0 ? 0 : 2;
+}
