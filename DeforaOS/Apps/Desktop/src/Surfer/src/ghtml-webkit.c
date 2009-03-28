4a99b82477182fa6e9f0076ef1610ff6f742e1f8
khorben <>
2009-03-28 11:14:49 +0000

    Initial release

---
+++ DeforaOS/Apps/Desktop/src/Surfer/src/ghtml-webkit.c
@@ -0,0 +1,21 @@
+/* $Id$ */
+/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Desktop Surfer */
+/* Surfer is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * Surfer is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Surfer; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA  02111-1307  USA */
+
+
+
+#include "ghtml.h"
+
+
+/* FIXME implement */
