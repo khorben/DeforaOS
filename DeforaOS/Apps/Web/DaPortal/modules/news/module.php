4e347ae827dde8ed6e0753848ad8a94ccc9c295e
khorben <>
2009-04-18 00:19:58 +0000

    Updating news requires admin privileges

--- DeforaOS/Apps/Web/DaPortal/modules/news/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/news/module.php
@@ -524,6 +524,9 @@ function news_update($args)
 
 	if(isset($error) && strlen($error))
 		_error($error);
+	require_once('./system/user.php');
+	if(!_user_admin($user_id))
+		return _error(PERMISSION_DENIED);
 	if(!isset($args['id']) || !is_numeric($args['id']))
 		return _error(INVALID_ARGUMENT);
 	require_once('./system/content.php');
