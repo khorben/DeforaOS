a7bca82de56f8e17269a36616bbda9ae17b5b908
khorben <>
2009-05-17 23:48:11 +0000

    Creates an ISO image for Linux (some values hard-coded)

--- DeforaOS/Apps/Devel/src/scripts/src/deforaos-build.sh
+++ DeforaOS/Apps/Devel/src/scripts/src/deforaos-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+#$Id$
 
 
 
@@ -7,12 +8,15 @@
 [ -z "$CVSROOT" ] && CVSROOT=":pserver:anonymous@cvs.defora.org:/Data/CVS"
 [ -z "$OS" ] && OS=`uname -s`
 #private
+DESTDIR="/var/www/htdocs/download/snapshots"
 DEVNULL="/dev/null"
 MODULE="DeforaOS"
+PREFIX="/usr"
 SRC="$HOME/build/$OS-$ARCH"
 DST="$HOME/destdir/$OS-$ARCH"
 
 #executables
+CP="cp -f"
 CVS="cvs -q"
 MAKE="make"
 MKDIR="mkdir -p"
@@ -33,6 +37,16 @@ error()
 
 
 #main
+#check variables
+case "$ARCH" in
+	i*86)
+		ARCH="i386"
+		;;
+	x86_64)
+		ARCH="amd64"
+		;;
+esac
+
 #configure cvs if necessary
 [ ! -f "$HOME/.cvspass" ] && touch "$HOME/.cvspass"
 
@@ -58,7 +72,26 @@ $MAKE DESTDIR="$DESTDIR"					|| error
 #build
 echo ""
 echo "Building CVS module $MODULE:"
-./build.sh MAKE="$MAKE" DESTDIR="$DST" install			|| error
+./build.sh MAKE="$MAKE" DESTDIR="$DST" PREFIX="$PREFIX" install	|| error
+
+#create CD-ROM image
+echo ""
+echo "Creating CD-ROM image:"
+./build.sh MAKE="$MAKE" DESTDIR="$DST" PREFIX="$PREFIX" \
+		IMAGE_TYPE="ramdisk" IMAGE_FILE="initrd.img" IMAGE_SIZE=8192 \
+		IMAGE_MODULES="/usr/src/linux-2.4.37/modules-ramdisk.tgz" \
+		image						|| error
+$RM -r "$DST"
+./build.sh MAKE="$MAKE" DESTDIR="$DST" PREFIX="$PREFIX" \
+		IMAGE_TYPE="iso" \
+		IMAGE_FILE="DeforaOS-daily.iso" \
+		IMAGE_KERNEL="/usr/src/linux-2.4.37/arch/i386/boot/bzImage" \
+		IMAGE_MODULES="/usr/src/linux-2.4.37/modules.tgz" \
+		IMAGE_RAMDISK="initrd.img" \
+		KERNEL_ARGS="vga=0x301 rw" \
+		image						|| error
+$CP "DeforaOS-daily.iso" "$DESTDIR"				|| error
+echo "http://www.defora.org/download/snapshots/DeforaOS-daily.tar.gz"
 
 #cleanup
 $RM -r "$SRC"
