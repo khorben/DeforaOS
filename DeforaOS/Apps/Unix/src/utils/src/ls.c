54aca2bc5f4e600069dc5d1e415e21204e3b64a2
khorben <>
2009-07-11 23:52:28 +0000

    Fixed a warning with -pedantic

--- DeforaOS/Apps/Unix/src/utils/src/ls.c
+++ DeforaOS/Apps/Unix/src/utils/src/ls.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Unix utils */
 /* utils is not free software; you can redistribute it and/or modify it under
  * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
@@ -689,8 +689,8 @@ static int _usage(void)
   -R	Recursively list subdirectories encountered\n\
   -a	Write out all hidden directory entries\n\
   -c	Use time of last modification of file status\n\
-  -d	Treat directories like files\n\
-  -l	Write out in long format\n\
+  -d	Treat directories like files\n", stderr);
+	fputs("  -l	Write out in long format\n\
   -t	Sort with the last modified file first\n\
   -u	Use time of last access\n\
   -1	Force output to be one entry per line\n\
