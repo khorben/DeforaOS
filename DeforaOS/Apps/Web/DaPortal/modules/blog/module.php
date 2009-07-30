08432b5c95f1473fa4e6e885f39049518443ae16
khorben <>
2009-07-30 11:58:48 +0000

    The TODO list was reduced

--- DeforaOS/Apps/Web/DaPortal/modules/blog/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/blog/module.php
@@ -15,8 +15,6 @@
 //along with DaPortal; if not, write to the Free Software
 //Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //FIXME
-//- create a specific table for blog posts (allow comments, ...)
-//- create a table for blogs (name, theme...)
 //- allow listing people's blogs
 
 
