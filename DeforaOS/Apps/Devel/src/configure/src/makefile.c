7accf952fa67b2d5469ebf7375e7c0f94706a5b2
khorben <>
2009-11-01 16:20:01 +0000

    Detect "-libname" just like "-l ibname" for platform-specific modifications

--- DeforaOS/Apps/Devel/src/configure/src/makefile.c
+++ DeforaOS/Apps/Devel/src/configure/src/makefile.c
@@ -527,6 +527,11 @@ static void _binary_ldflags(Configure * configure, FILE * fp,
 	{
 		snprintf(buf, sizeof(buf), "-l %s", libs[i]);
 		if((q = string_find(p, buf)) == NULL)
+		{
+			snprintf(buf, sizeof(buf), "-l%s", libs[i]);
+			q = string_find(p, buf);
+		}
+		if(q == NULL)
 			continue;
 		memmove(q, q + strlen(buf), strlen(q) - strlen(buf) + 1);
 	}
