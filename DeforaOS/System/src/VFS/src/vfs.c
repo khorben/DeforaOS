2eca6e456eb9119df94b3dc58519a0b2f2a19d24
khorben <>
2009-12-04 12:09:49 +0000

    Updated copyright information

--- DeforaOS/System/src/VFS/src/vfs.c
+++ DeforaOS/System/src/VFS/src/vfs.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2009 The DeforaOS Project */
+/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System VFS */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
