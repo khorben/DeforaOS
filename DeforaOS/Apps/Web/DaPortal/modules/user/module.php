731b6899b84ff159a4e40a52d1c994747858e549
khorben <>
2010-02-18 00:00:41 +0000

    Re-indenting

--- DeforaOS/Apps/Web/DaPortal/modules/user/module.php
+++ DeforaOS/Apps/Web/DaPortal/modules/user/module.php
@@ -406,7 +406,7 @@ function user_modify($args)
 	require_once('./system/user.php');
 	$admin = _user_admin($user_id) ? TRUE : FALSE;
 	if(isset($args['id']) && $admin == TRUE)
-			$id = $args['id'];
+		$id = $args['id'];
 	$user = _sql_array('SELECT user_id, username, enabled, admin, email'
 			.' FROM daportal_user WHERE user_id='."'$id'");
 	if(!is_array($user) || count($user) != 1)
