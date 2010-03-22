844840cd4bbe99ffe4542647592eee1f0627d2ed
khorben <>
2010-03-22 23:13:07 +0000

    The search module may now be configured

--- DeforaOS/Apps/Web/DaPortal/install/sqlite.sql
+++ DeforaOS/Apps/Web/DaPortal/install/sqlite.sql
@@ -35,6 +35,7 @@ INSERT INTO daportal_config (module_id, title, type, name, value_string) VALUES 
 INSERT INTO daportal_config (module_id, title, type, name, value_string) VALUES ('1', 'Default language', 'string', 'lang', 'en');
 INSERT INTO daportal_config (module_id, title, type, name, value_string) VALUES ('1', 'Default theme', 'string', 'theme', 'DaPortal');
 INSERT INTO daportal_config (module_id, title, type, name, value_string) VALUES ('1', 'Default title', 'string', 'title', 'DaPortal');
+INSERT INTO daportal_config (module_id, title, type, name, value_bool) VALUES ('4', 'Highlight search results', 'bool', 'title', FALSE);
 
 
 CREATE TABLE daportal_lang (
