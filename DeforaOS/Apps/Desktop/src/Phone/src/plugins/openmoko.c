450003c450940e01a9cf46422e253f1d8430fc39
khorben <>
2010-05-14 13:44:56 +0000

    Setting up the mixer levels on call start/stop/etc (untested)

--- DeforaOS/Apps/Desktop/src/Phone/src/plugins/openmoko.c
+++ DeforaOS/Apps/Desktop/src/Phone/src/plugins/openmoko.c
@@ -15,8 +15,21 @@
 
 
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <glib.h>
+#include <System.h>
 #include "Phone.h"
+#include "../../config.h"
+
+#ifndef PREFIX
+# define PREFIX		"/usr/local"
+#endif
+#ifndef DATADIR
+# define DATADIR	PREFIX "/share"
+#endif
 
 
 /* Openmoko */
@@ -37,15 +50,40 @@ PhonePlugin plugin =
 /* private */
 /* functions */
 /* openmoko_init */
+static int _event_mixer_set(char const * filename);
+
 static void _openmoko_event(PhoneEvent event)
 {
 	switch(event)
 	{
 		case PHONE_EVENT_CALL_ESTABLISHED:
+			_event_mixer_set("gsmhandset.xml");
+			break;
 		case PHONE_EVENT_CALL_INCOMING:
 		case PHONE_EVENT_CALL_OUTGOING:
+			/* FIXME is there anything to do? */
+			break;
 		case PHONE_EVENT_CALL_TERMINATED:
-			/* FIXME implement */
+			_event_mixer_set("speaker.xml");
 			break;
 	}
+}
+
+static int _event_mixer_set(char const * filename)
+{
+	char const scenarios[] = DATADIR "/openmoko/scenarios";
+	char * pathname;
+	size_t len;
+	char * alsactl[] = { "/usr/sbin/alsactl", "alsactl", "-f", NULL,
+		"restore", NULL };
+
+	len = sizeof(scenarios) + 1 + strlen(filename);
+	if((pathname = malloc(len)) == NULL)
+		return error_set_code(1, "%s", strerror(errno));
+	snprintf(pathname, len, "%s/%s", scenarios, filename);
+	alsactl[3] = pathname;
+	g_spawn_async(NULL, alsactl, NULL, G_SPAWN_FILE_AND_ARGV_ZERO,
+			NULL, NULL, NULL, NULL);
+	free(pathname);
+	return 0;
 }
