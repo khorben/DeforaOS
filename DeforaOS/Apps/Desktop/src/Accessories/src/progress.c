42f8d674bafd4e08b56aaa9140ba7f919ced159d
khorben <>
2010-07-04 11:37:11 +0000

    Let errors be modal

--- DeforaOS/Apps/Desktop/src/Accessories/src/progress.c
+++ DeforaOS/Apps/Desktop/src/Accessories/src/progress.c
@@ -62,6 +62,7 @@ typedef struct _Progress
 	guint out_id;
 
 	/* widgets */
+	GtkWidget * window;
 	GtkWidget * done;
 	GtkWidget * speed;
 	GtkWidget * progress;
@@ -69,8 +70,9 @@ typedef struct _Progress
 } Progress;
 
 /* functions */
-static int _progress_error(char const * message, int ret);
-static int _progress_gerror(char const * message, GError * error, int ret);
+static int _progress_error(Progress * progress, char const * message, int ret);
+static int _progress_gerror(Progress * progress, char const * message,
+		GError * error, int ret);
 static int _progress_exec(Progress * progress, char * argv[]);
 
 /* callbacks */
@@ -87,7 +89,6 @@ static int _progress(Prefs * prefs, char * argv[])
 {
 	Progress p;
 	struct stat st;
-	GtkWidget * window = NULL;
 	GtkWidget * vbox;
 	GtkWidget * hbox;
 	GtkSizeGroup * left;
@@ -99,25 +100,25 @@ static int _progress(Prefs * prefs, char * argv[])
 	memset(&p, 0, sizeof(p));
 	p.prefs = prefs;
 	if((p.buf = malloc(prefs->bufsiz)) == NULL)
-		return _progress_error("malloc", 1);
+		return _progress_error(&p, "malloc", 1);
 	p.bufsiz = prefs->bufsiz;
 	if(pipe(p.fds) != 0)
-		return _progress_error("pipe", 1);
+		return _progress_error(&p, "pipe", 1);
 	if((p.pid = fork()) == -1)
 	{
 		close(p.fds[0]);
 		close(p.fds[1]);
-		return _progress_error("fork", 1);
+		return _progress_error(&p, "fork", 1);
 	}
 	if(p.pid != 0)
 		return _progress_exec(&p, argv);
 	close(p.fds[0]);
 	if(gettimeofday(&p.tv, NULL) != 0)
-		return _progress_error("gettimeofday", 1);
+		return _progress_error(&p, "gettimeofday", 1);
 	if(prefs->filename == NULL)
 		prefs->filename = "Standard input";
 	else if((p.fd = open(prefs->filename, O_RDONLY)) < 0)
-		return _progress_error(prefs->filename, 1);
+		return _progress_error(&p, prefs->filename, 1);
 	else if(fstat(p.fd, &st) == 0)
 		prefs->length = st.st_size;
 	p.in_channel = g_io_channel_unix_new(p.fd);
@@ -128,11 +129,11 @@ static int _progress(Prefs * prefs, char * argv[])
 	g_io_channel_set_encoding(p.out_channel, NULL, NULL);
 	p.out_id = 0;
 	/* graphical interface */
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_default_size(GTK_WINDOW(window), 300, 100);
-	gtk_window_set_title(GTK_WINDOW(window), prefs->title != NULL
+	p.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size(GTK_WINDOW(p.window), 300, 100);
+	gtk_window_set_title(GTK_WINDOW(p.window), prefs->title != NULL
 			? prefs->title : "Progress");
-	g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(
+	g_signal_connect(G_OBJECT(p.window), "delete-event", G_CALLBACK(
 				_progress_closex), NULL);
 	vbox = gtk_vbox_new(FALSE, 0);
 	hbox = gtk_hbox_new(FALSE, 0);
@@ -190,30 +191,34 @@ static int _progress(Prefs * prefs, char * argv[])
 				_progress_cancel), NULL);
 	gtk_box_pack_end(GTK_BOX(hbox), widget, FALSE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), hbox, TRUE, TRUE, 0);
-	gtk_container_add(GTK_CONTAINER(window), vbox);
-	gtk_container_set_border_width(GTK_CONTAINER(window), 4);
-	gtk_widget_show_all(window);
+	gtk_container_add(GTK_CONTAINER(p.window), vbox);
+	gtk_container_set_border_width(GTK_CONTAINER(p.window), 4);
+	gtk_widget_show_all(p.window);
 	gtk_main();
 	close(p.fd);
 	close(p.fds[1]);
 	return 0;
 }
 
-static int _error_do(char const * message, char const * error, int ret);
+static int _error_do(Progress * progress, char const * message,
+		char const * error, int ret);
 
-static int _progress_error(char const * message, int ret)
+static int _progress_error(Progress * progress, char const * message,
+		int ret)
 {
-	return _error_do(message, strerror(errno), ret);
+	return _error_do(progress, message, strerror(errno), ret);
 }
 
-static int _progress_gerror(char const * message, GError * error, int ret)
+static int _progress_gerror(Progress * progress, char const * message,
+		GError * error, int ret)
 {
-	_error_do(message, error->message, ret);
+	_error_do(progress, message, error->message, ret);
 	g_error_free(error);
 	return ret;
 }
 
-static int _error_do(char const * message, char const * error, int ret)
+static int _error_do(Progress * progress, char const * message,
+		char const * error, int ret)
 {
 	GtkWidget * dialog;
 
@@ -223,7 +228,9 @@ static int _error_do(char const * message, char const * error, int ret)
 		perror(message);
 		return -ret;
 	}
-	dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT,
+	dialog = gtk_message_dialog_new((progress != NULL)
+			? GTK_WINDOW(progress->window) : NULL,
+			GTK_DIALOG_DESTROY_WITH_PARENT,
 			GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "%s: %s", message,
 			error);
 	gtk_window_set_title(GTK_WINDOW(dialog), "Error");
@@ -274,7 +281,7 @@ static int _progress_exec(Progress * progress, char * argv[])
 		_exec_gunzip(progress, argv);
 	else
 		execvp(argv[0], argv);
-	exit(_progress_error(argv[0], -1));
+	exit(_progress_error(NULL, argv[0], -1));
 	return 1;
 }
 
@@ -285,17 +292,17 @@ static int _exec_gunzip(Progress * progress, char * argv[])
 	tmp.prefs = progress->prefs;
 	tmp.prefs->flags -= PREFS_z;
 	if(pipe(tmp.fds) != 0)
-		exit(_progress_error("pipe", -1));
+		exit(_progress_error(NULL, "pipe", -1));
 	if((tmp.pid = fork()) == -1)
-		exit(_progress_error("fork", -1));
+		exit(_progress_error(NULL, "fork", -1));
 	if(tmp.pid == 0)
 		return _progress_exec(&tmp, argv);
 	close(tmp.fds[0]);
 	if(dup2(tmp.fds[1], 1) == -1)
-		exit(_progress_error("dup2", -1));
+		exit(_progress_error(NULL, "dup2", -1));
 	execlp("gunzip", "gunzip", NULL);
 	execlp("gzip", "gzip", "-d", NULL);
-	exit(_progress_error("gunzip", -1));
+	exit(_progress_error(NULL, "gunzip", -1));
 	return 1;
 }
 
@@ -329,7 +336,7 @@ static gboolean _progress_channel(GIOChannel * source, GIOCondition condition,
 		return _channel_in(p, source);
 	if(condition == G_IO_OUT)
 		return _channel_out(p, source);
-	_progress_error(p->prefs->filename, 0);
+	_progress_error(p, p->prefs->filename, 0);
 	gtk_main_quit();
 	return FALSE;
 }
@@ -348,7 +355,7 @@ static gboolean _channel_in(Progress * p, GIOChannel * source)
 			p->bufsiz - p->buf_cnt, &read, &error);
 	if(status == G_IO_STATUS_ERROR)
 	{
-		_progress_error(p->prefs->filename, 0);
+		_progress_error(p, p->prefs->filename, 0);
 		g_io_channel_close(source);
 		gtk_main_quit();
 		return FALSE;
@@ -381,7 +388,7 @@ static gboolean _channel_out(Progress * p, GIOChannel * source)
 				&error);
 	if(status == G_IO_STATUS_ERROR)
 	{
-		_progress_gerror(p->prefs->filename, error, 0);
+		_progress_gerror(p, p->prefs->filename, error, 0);
 		gtk_main_quit();
 		return FALSE;
 	}
@@ -471,7 +478,7 @@ static void _timeout_speed(Progress * progress)
 
 	if(gettimeofday(&tv, NULL) != 0)
 	{
-		_progress_error("gettimeofday", FALSE);
+		_progress_error(progress, "gettimeofday", FALSE);
 		return;
 	}
 	if((tv.tv_sec = tv.tv_sec - progress->tv.tv_sec) < 0)
