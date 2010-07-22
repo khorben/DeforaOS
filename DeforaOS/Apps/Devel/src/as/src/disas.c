e15b2ed060d9fca7e6cdcc9931806f5ed8366253
khorben <>
2010-07-22 02:31:48 +0000

    Re-structured trying to allow format plug-ins to be reused

--- DeforaOS/Apps/Devel/src/as/src/disas.c
+++ DeforaOS/Apps/Devel/src/as/src/disas.c
@@ -38,6 +38,27 @@ typedef struct _Disas
 	Arch * arch;
 } Disas;
 
+typedef struct _DisasSignature
+{
+	char const * name;
+	char const * signature;
+	size_t size;
+	int (*callback)(Disas * disas);
+} DisasSignature;
+
+
+/* variables */
+static int _disas_elf(Disas * disas);
+static int _disas_flat(Disas * disas);
+
+static DisasSignature _disas_signatures[] =
+{
+	{ "elf",	ELFMAG, SELFMAG,	_disas_elf	},
+	{ "flat",	NULL, 0,		_disas_flat	}
+};
+#define _disas_signatures_cnt (sizeof(_disas_signatures) \
+		/ sizeof(*_disas_signatures))
+
 
 /* prototypes */
 static int _disas_error(char const * message, int ret);
@@ -46,24 +67,7 @@ static int _disas_error(char const * message, int ret);
 /* functions */
 /* disas */
 static int _disas_do(Disas * disas);
-
-/* ELF */
-static int _do_elf(Disas * disas);
-/* ELF32 */
-static int _do_elf32(Disas * disas, Elf32_Ehdr * ehdr);
-static Elf32_Shdr * _do_elf32_shdr(char const * filename, FILE * fp,
-		Elf32_Ehdr * ehdr);
-static int _do_elf32_strtab(Disas * disas, Elf32_Shdr * shdr, size_t shdr_cnt,
-		uint16_t ndx, char ** strtab, size_t * strtab_cnt);
-/* ELF64 */
-static int _do_elf64(Disas * disas, Elf64_Ehdr * ehdr);
-static Elf64_Shdr * _do_elf64_shdr(char const * filename, FILE * fp,
-		Elf64_Ehdr * ehdr);
-static int _do_elf64_strtab(Disas * disas, Elf64_Shdr * shdr, size_t shdr_cnt,
-		uint16_t ndx, char ** strtab, size_t * strtab_cnt);
-
-/* Flat */
-static int _do_flat(Disas * disas, size_t offset, size_t size);
+static int _do_flat(Disas * disas, off_t offset, size_t size);
 static int _do_flat_print(Disas * disas, ArchInstruction * ai);
 
 static int _disas(char const * arch, char const * filename)
@@ -82,24 +86,142 @@ static int _disas(char const * arch, char const * filename)
 static int _disas_do(Disas * disas)
 {
 	int ret = 1;
-	char buf[32];
-	size_t s;
-	struct stat st;
+	size_t i;
+	size_t s = 0;
+	char * buf;
 
 	if((disas->fp = fopen(disas->filename, "r")) == NULL)
 		return _disas_error(disas->filename, 1);
-	s = fread(buf, sizeof(*buf), sizeof(buf), disas->fp);
-	if(s > SELFMAG && memcmp(ELFMAG, buf, SELFMAG) == 0)
-		ret = _do_elf(disas);
-	else if(fstat(fileno(disas->fp), &st) != 0)
-		ret = _disas_error(disas->filename, 1);
-	else
-		ret = _do_flat(disas, 0, st.st_size);
+	for(i = 0; i < _disas_signatures_cnt; i++)
+		if(_disas_signatures[i].size > s)
+			s = _disas_signatures[i].size;
+	if((buf = malloc(s)) == NULL
+			|| fread(buf, sizeof(*buf), s, disas->fp) != s)
+	{
+		free(buf);
+		fclose(disas->fp);
+		return _disas_error(disas->filename, 1);
+	}
+	for(i = 0; i < _disas_signatures_cnt; i++)
+		if(memcmp(_disas_signatures[i].signature, buf,
+					_disas_signatures[i].size) == 0)
+		{
+			ret = _disas_signatures[i].callback(disas);
+			break;
+		}
+	free(buf);
 	fclose(disas->fp);
 	return ret;
 }
 
-static int _do_elf(Disas * disas)
+static int _do_flat(Disas * disas, off_t offset, size_t size)
+{
+	int ret = 0;
+	size_t i;
+	unsigned int opcode;
+	size_t j;
+	int c;
+	ArchInstruction * ai;
+
+#ifdef DEBUG
+	fprintf(stderr, "DEBUG: %s(%p, 0x%zx, 0x%zx) arch=\"%s\"\n", __func__,
+			disas, offset, size, as_get_arch(disas->as));
+#endif
+	if(fseek(disas->fp, offset, SEEK_SET) != 0)
+		return _disas_error(disas->filename, 1);
+	if((disas->arch = arch_new(as_get_arch(disas->as))) == NULL)
+		return error_print("disas");
+	printf("\n%08zx:\n", offset);
+	for(i = 0; i < size; i++)
+	{
+		printf("%5zx: ", i);
+		opcode = 0;
+		ret = 0;
+		for(j = 0; j < 4; j++)
+		{
+			if((c = fgetc(disas->fp)) == EOF)
+				break;
+			printf(" %02x", c);
+			opcode = (opcode << 8) | c;
+			if((ai = arch_instruction_get_by_opcode(disas->arch,
+							j + 1, opcode)) == NULL)
+				continue;
+			ret = _do_flat_print(disas, ai);
+			j++;
+			break;
+		}
+		fputc('\n', stdout);
+		if(ret < 0)
+			break;
+		i += ret + j - 1;
+	}
+	arch_delete(disas->arch);
+	return -ret;
+}
+
+static int _do_flat_print(Disas * disas, ArchInstruction * ai)
+{
+	int ret = 0;
+	int i;
+	ArchOperands operands;
+	unsigned int reg;
+	ArchRegister * ar;
+	char const * sep = " ";
+	uint8_t size;
+	int j;
+	unsigned long u;
+	int c;
+
+	printf("\t\t%s", ai->name);
+	for(i = 0, operands = ai->operands; operands > 0; i++, operands >>= 8)
+		if((operands & _AO_OP) == _AO_REG)
+		{
+			reg = (operands & 0xff) >> 2;
+			if((ar = arch_register_get_by_id(disas->arch, reg))
+					!= NULL)
+				printf("%s%%%s", sep, ar->name);
+			else
+				printf("%s%d", sep, reg);
+			sep = ", ";
+		}
+		else if((operands & _AO_OP) == _AO_IMM)
+		{
+			size = (i == 0) ? ai->op1size : ((i == 1) ? ai->op2size
+					: ai->op3size);
+			for(j = 0, u = 0; j < size; j++)
+			{
+				if((c = fgetc(disas->fp)) == EOF)
+				{
+					ret = _disas_error(disas->filename, -1);
+					break;
+				}
+				u = (u << 8) | c; /* XXX endian */
+			}
+			if(j != size)
+				break;
+			printf("%s$0x%lx", sep, u);
+			sep = ", ";
+			ret += size;
+		}
+	return ret;
+}
+
+
+/* disas_elf */
+/* ELF32 */
+static int _do_elf32(Disas * disas, Elf32_Ehdr * ehdr);
+static Elf32_Shdr * _do_elf32_shdr(char const * filename, FILE * fp,
+		Elf32_Ehdr * ehdr);
+static int _do_elf32_strtab(Disas * disas, Elf32_Shdr * shdr, size_t shdr_cnt,
+		uint16_t ndx, char ** strtab, size_t * strtab_cnt);
+/* ELF64 */
+static int _do_elf64(Disas * disas, Elf64_Ehdr * ehdr);
+static Elf64_Shdr * _do_elf64_shdr(char const * filename, FILE * fp,
+		Elf64_Ehdr * ehdr);
+static int _do_elf64_strtab(Disas * disas, Elf64_Shdr * shdr, size_t shdr_cnt,
+		uint16_t ndx, char ** strtab, size_t * strtab_cnt);
+
+static int _disas_elf(Disas * disas)
 {
 	union {
 		unsigned char e_ident[EI_NIDENT];
@@ -284,96 +406,15 @@ static int _do_elf64_strtab(Disas * disas, Elf64_Shdr * shdr, size_t shdr_cnt,
 	return 0;
 }
 
-static int _do_flat(Disas * disas, size_t offset, size_t size)
-{
-	int ret = 0;
-	size_t i;
-	unsigned int opcode;
-	size_t j;
-	int c;
-	ArchInstruction * ai;
 
-#ifdef DEBUG
-	fprintf(stderr, "DEBUG: %s(%p, 0x%zx, 0x%zx) arch=\"%s\"\n", __func__,
-			disas, offset, size, as_get_arch(disas->as));
-#endif
-	if(fseek(disas->fp, offset, SEEK_SET) != 0)
+/* disas_flat */
+static int _disas_flat(Disas * disas)
+{
+	struct stat st;
+
+	if(fstat(fileno(disas->fp), &st) != 0)
 		return _disas_error(disas->filename, 1);
-	if((disas->arch = arch_new(as_get_arch(disas->as))) == NULL)
-		return error_print("disas");
-	printf("\n%08zx:\n", offset);
-	for(i = 0; i < size; i++)
-	{
-		printf("%5zx: ", i);
-		opcode = 0;
-		ret = 0;
-		for(j = 0; j < 4; j++)
-		{
-			if((c = fgetc(disas->fp)) == EOF)
-				break;
-			printf(" %02x", c);
-			opcode = (opcode << 8) | c;
-			if((ai = arch_instruction_get_by_opcode(disas->arch,
-							j + 1, opcode)) == NULL)
-				continue;
-			ret = _do_flat_print(disas, ai);
-			j++;
-			break;
-		}
-		fputc('\n', stdout);
-		if(ret < 0)
-			break;
-		i += ret + j - 1;
-	}
-	arch_delete(disas->arch);
-	return -ret;
-}
-
-static int _do_flat_print(Disas * disas, ArchInstruction * ai)
-{
-	int ret = 0;
-	int i;
-	ArchOperands operands;
-	unsigned int reg;
-	ArchRegister * ar;
-	char const * sep = " ";
-	uint8_t size;
-	int j;
-	unsigned long u;
-	int c;
-
-	printf("\t\t%s", ai->name);
-	for(i = 0, operands = ai->operands; operands > 0; i++, operands >>= 8)
-		if((operands & _AO_OP) == _AO_REG)
-		{
-			reg = (operands & 0xff) >> 2;
-			if((ar = arch_register_get_by_id(disas->arch, reg))
-					!= NULL)
-				printf("%s%%%s", sep, ar->name);
-			else
-				printf("%s%d", sep, reg);
-			sep = ", ";
-		}
-		else if((operands & _AO_OP) == _AO_IMM)
-		{
-			size = (i == 0) ? ai->op1size : ((i == 1) ? ai->op2size
-					: ai->op3size);
-			for(j = 0, u = 0; j < size; j++)
-			{
-				if((c = fgetc(disas->fp)) == EOF)
-				{
-					ret = _disas_error(disas->filename, -1);
-					break;
-				}
-				u = (u << 8) | c; /* XXX endian */
-			}
-			if(j != size)
-				break;
-			printf("%s$0x%lx", sep, u);
-			sep = ", ";
-			ret += size;
-		}
-	return ret;
+	return _do_flat(disas, 0, st.st_size);
 }
 
 
