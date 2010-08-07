25631121a767f0be64d90788596082131e22df16
khorben <>
2010-08-07 22:25:39 +0000

    About to release version 0.0.2

--- DeforaOS/Apps/Desktop/src/Mixer/Makefile
+++ DeforaOS/Apps/Desktop/src/Mixer/Makefile
@@ -1,5 +1,5 @@
 PACKAGE	= Mixer
-VERSION	= 0.0.1
+VERSION	= 0.0.2
 SUBDIRS	= data po src
 RM	= rm -f
 LN	= ln -f
--- DeforaOS/Apps/Desktop/src/Mixer/config.sh
+++ DeforaOS/Apps/Desktop/src/Mixer/config.sh
@@ -1,5 +1,5 @@
 PACKAGE="Mixer"
-VERSION="0.0.1"
+VERSION="0.0.2"
 
 PREFIX="/usr/local"
 LIBDIR="${PREFIX}/lib"
