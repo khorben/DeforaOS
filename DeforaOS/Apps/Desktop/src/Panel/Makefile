61204aa84e0c23219afb3e5d0fb9445cca8d8695
khorben <>
2010-08-26 16:28:10 +0000

    About to release version 0.1.1

--- DeforaOS/Apps/Desktop/src/Panel/Makefile
+++ DeforaOS/Apps/Desktop/src/Panel/Makefile
@@ -1,5 +1,5 @@
 PACKAGE	= Panel
-VERSION	= 0.1.0
+VERSION	= 0.1.1
 SUBDIRS	= include po src
 RM	= rm -f
 LN	= ln -f
--- DeforaOS/Apps/Desktop/src/Panel/config.h
+++ DeforaOS/Apps/Desktop/src/Panel/config.h
@@ -1,5 +1,5 @@
 #define PACKAGE "Panel"
-#define VERSION "0.1.0"
+#define VERSION "0.1.1"
 
 #ifndef PREFIX
 # define PREFIX "/usr/local"
