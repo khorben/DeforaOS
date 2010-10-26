cfb23feef7d664bdae1557dc8a422a0eff538d71
khorben <>
2010-10-26 15:40:13 +0000

    Releasing Phone 0.2.3

--- DeforaOS/Apps/Desktop/src/Phone/config.h
+++ DeforaOS/Apps/Desktop/src/Phone/config.h
@@ -1,5 +1,5 @@
 #define PACKAGE "Phone"
-#define VERSION "0.2.2"
+#define VERSION "0.2.3"
 
 #ifndef PREFIX
 # define PREFIX "/usr/local"
--- DeforaOS/Apps/Desktop/src/Phone/config.sh
+++ DeforaOS/Apps/Desktop/src/Phone/config.sh
@@ -1,5 +1,5 @@
 PACKAGE="Phone"
-VERSION="0.2.2"
+VERSION="0.2.3"
 
 PREFIX="/usr/local"
 LIBDIR="${PREFIX}/lib"
