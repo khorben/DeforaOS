5bd1806ece988b37f3a79a67776a9c072313fa1f
khorben <>
2010-11-15 02:44:25 +0000

    Initial revision

---
+++ DeforaOS/Apps/Desktop/src/Surfer/src/download.h
@@ -0,0 +1,41 @@
+/* $Id$ */
+/* Copyright (c) 2010 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS Desktop Surfer */
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+
+
+#ifndef SURFER_DOWNLOAD_H
+# define SURFER_DOWNLOAD_H
+
+
+/* Download */
+/* types */
+typedef struct _Download Download;
+
+typedef struct _DownloadPrefs
+{
+	char * output;
+	char * user_agent;
+} DownloadPrefs;
+
+
+/* functions */
+Download * download_new(DownloadPrefs * prefs, char const * url);
+void download_delete(Download * download);
+
+
+/* useful */
+int download_cancel(Download * download);
+
+#endif /* !SURFER_DOWNLOAD_H */
