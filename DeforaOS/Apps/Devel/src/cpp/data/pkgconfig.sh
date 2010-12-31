28b97fb5c542530e6ee36f3070b32688c3568bce
khorben <>
2010-12-31 16:46:43 +0000

    New way of running configuration scripts during compilation

--- DeforaOS/Apps/Devel/src/cpp/data/pkgconfig.sh
+++ DeforaOS/Apps/Devel/src/cpp/data/pkgconfig.sh
@@ -24,32 +24,58 @@ _debug()
 #usage
 _usage()
 {
-	echo "Usage: pkgconfig.sh target" 1>&2
-	echo "       pkgconfig.sh -p prefix install target" 1>&2
-	echo "       pkgconfig.sh -p prefix uninstall target" 1>&2
+	echo "Usage: pkgconfig.sh [-i|-u][-P prefix] target" 1>&2
 	return 1
 }
 
 
 #main
-if [ $# -eq 4 -a "$1" = "-p" ]; then
-	PREFIX="$2"
-	PKGCONFIG="$PREFIX/lib/pkgconfig"
-
-	if [ "$3" = "install" ]; then
-		$DEBUG $MKDIR "$PKGCONFIG"			|| exit 2
-		$DEBUG $INSTALL "$4" "$PKGCONFIG/$4"		|| exit 2
-		exit 0
-	elif [ "$3" = "uninstall" ]; then
-		$DEBUG $RM "$PKGCONFIG/$4"			|| exit 2
-		exit 0
-	else
-		echo "pkgconfig.sh: $3: Unknown operation" 1>&2
-	fi
-fi
-if [ $# -ne 1 ]; then
+args=`getopt iuP: $*`
+if [ $? -ne 0 ]; then
 	_usage
 	exit $?
 fi
+set -- $args
+install=0
+uninstall=0
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-i)
+			install=1
+			;;
+		-u)
+			uninstall=1
+			;;
+		-P)
+			PREFIX="$2"
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+	shift
+done
 
-$SED -e "s,PREFIX,$PREFIX," -e "s,VERSION,$VERSION," "$1.in" > "$1"
+PKGCONFIG="$PREFIX/lib/pkgconfig"
+while [ $# -gt 0 ]; do
+	target="$1"
+	shift
+
+	#uninstall
+	if [ "$uninstall" -eq 1 ]; then
+		$DEBUG $RM "$PKGCONFIG/$target"			|| exit 2
+		continue
+	fi
+
+	#create
+	$SED -e "s,PREFIX,$PREFIX," -e "s,VERSION,$VERSION," "$target.in" \
+		> "$target"					|| exit 2
+
+	#install
+	if [ "$install" -eq 1 ]; then
+		$DEBUG $MKDIR "$PKGCONFIG"			|| exit 2
+		$DEBUG $INSTALL "$target" "$PKGCONFIG/$target"	|| exit 2
+	fi
+done
