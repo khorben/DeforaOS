a3921adf654dd787cfe0f32deeade3a17c4ecb15
khorben <>
2011-01-03 00:22:22 +0000

    Added missing include file

--- DeforaOS/Apps/Devel/src/GEDI/src/main.c
+++ DeforaOS/Apps/Devel/src/GEDI/src/main.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2010 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2011 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Devel GEDI */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 
 
 #include <unistd.h>
+#include <stdio.h>
 #include "gedi.h"
 
 
