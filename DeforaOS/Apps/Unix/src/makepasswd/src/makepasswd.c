36b0bc11f4da07f3784e410203cfd589d3608986
khorben <>
2011-01-04 01:40:55 +0000

    Added support for SHA512 on Linux

--- DeforaOS/Apps/Unix/src/makepasswd/src/makepasswd.c
+++ DeforaOS/Apps/Unix/src/makepasswd/src/makepasswd.c
@@ -47,7 +47,7 @@ typedef struct _Prefs
 typedef enum _PrefsEncryption
 {
 	PE_NONE = 0, PE_BASE64, PE_BLOWFISH, PE_DES, PE_MD5, PE_SHA1, PE_SHA256,
-	PE_SHMD5
+	PE_SHA512, PE_SHMD5
 } PrefsEncryption;
 #define PE_LAST PE_SHMD5
 #define PE_COUNT (PE_LAST + 1)
@@ -57,8 +57,8 @@ typedef enum _PrefsEncryption
 /* options */
 static const char * _encryption_strings[PE_COUNT + 1] =
 {
-	"none", "base64", "blowfish", "des", "md5", "sha1", "sha256", "shmd5",
-	NULL
+	"none", "base64", "blowfish", "des", "md5", "sha1", "sha256", "sha512",
+	"shmd5", NULL
 };
 
 
@@ -151,6 +151,7 @@ static char * _hash_none(void);
 static char * _hash_sha1(char const * password, char const * salt,
 		int iterations);
 static char * _hash_sha256(char const * password, char const * salt);
+static char * _hash_sha512(char const * password, char const * salt);
 static char * _hash_shmd5(char const * password, char const * salt);
 
 static char * _makepasswd_hash(PrefsEncryption encryption,
@@ -172,6 +173,8 @@ static char * _makepasswd_hash(PrefsEncryption encryption,
 			return _hash_sha1(password, salt, iterations);
 		case PE_SHA256:
 			return _hash_sha256(password, salt);
+		case PE_SHA512:
+			return _hash_sha512(password, salt);
 		case PE_SHMD5:
 			return _hash_shmd5(password, salt);
 		default:
@@ -403,7 +406,41 @@ static char * _hash_sha256(char const * password, char const * salt)
 
 	if(salt == NULL)
 	{
-		if((p = _makepasswd_password(characters, 8, 64)) == NULL)
+		if((p = _makepasswd_password(characters, 16, 16)) == NULL)
+			return NULL;
+		salt = p;
+	}
+	len = sizeof(prefix) + strlen(salt);
+	if((s = malloc(len)) == NULL)
+	{
+		_error("malloc", 1);
+		free(p);
+		return NULL;
+	}
+	snprintf(s, len, "%s%s", prefix, salt);
+	ret = crypt(password, s);
+	free(p);
+	free(s);
+	if(ret == NULL)
+		_error("crypt", 1);
+	else if((ret = strdup(ret)) == NULL)
+		_error("malloc", 1);
+	return ret;
+}
+
+static char * _hash_sha512(char const * password, char const * salt)
+{
+	const char prefix[] = "$6$";
+	const char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz0123456789./";
+	char * ret;
+	char * p = NULL;
+	size_t len;
+	char * s;
+
+	if(salt == NULL)
+	{
+		if((p = _makepasswd_password(characters, 16, 16)) == NULL)
 			return NULL;
 		salt = p;
 	}
