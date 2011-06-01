4f939073144e1874a7314accbd6ea1f2fe9a5fd7
khorben <>
2011-06-01 13:35:08 +0000

    Using the "getopts" shell builtin instead of the "getopt" shell utility

--- DeforaOS/System/src/Splasher/src/appbroker.sh
+++ DeforaOS/System/src/Splasher/src/appbroker.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 #$Id$
+#Copyright (c) 2011 Pierre Pronchery <khorben@defora.org>
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 
+#functions
 #usage
 _usage()
 {
@@ -12,29 +35,22 @@ _usage()
 
 
 #main
-args=`getopt P: $*`
-if [ $? -ne 0 ]; then
-	_usage
-	exit $?
-fi
-set -- $args
-while [ $# -gt 0 ]; do
-	case "$1" in
-		-P)
+while getopts P: name; do
+	case "$name" in
+		P)
 			#we can ignore it
-			shift
 			;;
-		--)
-			shift
-			break
+		?)
+			_usage
+			exit $?
 			;;
 	esac
-	shift
 done
-
+shift $(($OPTIND - 1))
 if [ $# -ne 1 ]; then
 	_usage
 	exit $?
 fi
+
 APPINTERFACE="${1%%.h}.interface"
 AppBroker -o "$1" "$APPINTERFACE"
