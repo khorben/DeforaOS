0a3179a5058497b78f34863e7f1c303e397c8889
khorben <>
2011-07-25 10:37:35 +0000

    The CVS plug-in committing itself

--- DeforaOS/Apps/Desktop/src/Browser/src/plugins/cvs.c
+++ DeforaOS/Apps/Desktop/src/Browser/src/plugins/cvs.c
@@ -28,6 +28,8 @@
 /* CVS */
 /* private */
 /* types */
+typedef struct _CVSTask CVSTask;
+
 typedef struct _CVS
 {
 	char * filename;
@@ -46,13 +48,55 @@ typedef struct _CVS
 	/* file */
 	GtkWidget * file;
 	GtkWidget * f_revision;
+
+	/* tasks */
+	CVSTask ** tasks;
+	size_t tasks_cnt;
 } CVS;
+
+struct _CVSTask
+{
+	BrowserPlugin * plugin;
+
+	GPid pid;
+
+	/* stdout */
+	gint o_fd;
+	GIOChannel * o_channel;
+	guint o_source;
+
+	/* stderr */
+	gint e_fd;
+	GIOChannel * e_channel;
+	guint e_source;
+
+	/* widgets */
+	GtkWidget * window;
+	GtkWidget * view;
+};
 
 
 /* prototypes */
 static GtkWidget * _cvs_init(BrowserPlugin * plugin);
 static void _cvs_destroy(BrowserPlugin * plugin);
 static void _cvs_refresh(BrowserPlugin * plugin, char const * path);
+
+static int _cvs_add_task(BrowserPlugin * plugin, char const * directory,
+		char * argv[]);
+
+/* tasks */
+static void _cvs_task_delete(CVSTask * task);
+static void _cvs_task_close(CVSTask * task);
+static void _cvs_task_close_channel(CVSTask * task, GIOChannel * channel);
+
+/* callbacks */
+static void _cvs_on_commit(gpointer data);
+static void _cvs_on_diff(gpointer data);
+static void _cvs_on_update(gpointer data);
+/* tasks */
+static gboolean _cvs_task_on_closex(gpointer data);
+static gboolean _cvs_task_on_io_can_read(GIOChannel * channel,
+		GIOCondition condition, gpointer data);
 
 
 /* public */
@@ -72,6 +116,8 @@ BrowserPlugin plugin =
 
 /* private */
 /* functions */
+static GtkWidget * _init_button(GtkSizeGroup * group, char const * label,
+		GCallback callback, gpointer data);
 static GtkWidget * _init_label(GtkSizeGroup * group, char const * label,
 		GtkWidget ** widget);
 
@@ -86,6 +132,8 @@ static GtkWidget * _cvs_init(BrowserPlugin * plugin)
 		return NULL;
 	plugin->priv = cvs;
 	cvs->filename = NULL;
+	cvs->source = 0;
+	/* widgets */
 	cvs->widget = gtk_vbox_new(FALSE, 4);
 	font = pango_font_description_new();
 	pango_font_description_set_weight(font, PANGO_WEIGHT_BOLD);
@@ -114,15 +162,41 @@ static GtkWidget * _cvs_init(BrowserPlugin * plugin)
 			0);
 	/* file */
 	cvs->file = gtk_vbox_new(FALSE, 4);
+	group = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
 	widget = _init_label(group, _("Revision:"), &cvs->f_revision);
+	gtk_box_pack_start(GTK_BOX(cvs->file), widget, FALSE, TRUE, 0);
+	widget = _init_button(group, _("Request diff"), G_CALLBACK(
+				_cvs_on_diff), plugin);
+	gtk_box_pack_start(GTK_BOX(cvs->file), widget, FALSE, TRUE, 0);
+	widget = _init_button(group, _("Update"), G_CALLBACK(_cvs_on_update),
+			plugin);
+	gtk_box_pack_start(GTK_BOX(cvs->file), widget, FALSE, TRUE, 0);
+	widget = _init_button(group, _("Commit"), G_CALLBACK(_cvs_on_commit),
+			plugin);
 	gtk_box_pack_start(GTK_BOX(cvs->file), widget, FALSE, TRUE, 0);
 	gtk_widget_show_all(cvs->file);
 	gtk_widget_set_no_show_all(cvs->file, TRUE);
 	gtk_box_pack_start(GTK_BOX(cvs->widget), cvs->file, FALSE, TRUE, 0);
 	gtk_widget_show_all(cvs->widget);
-	cvs->source = 0;
 	pango_font_description_free(font);
+	/* tasks */
+	cvs->tasks = NULL;
+	cvs->tasks_cnt = 0;
 	return cvs->widget;
+}
+
+static GtkWidget * _init_button(GtkSizeGroup * group, char const * label,
+		GCallback callback, gpointer data)
+{
+	GtkWidget * hbox;
+	GtkWidget * widget;
+
+	hbox = gtk_hbox_new(FALSE, 4);
+	widget = gtk_button_new_with_label(label);
+	gtk_size_group_add_widget(group, widget);
+	g_signal_connect_swapped(widget, "clicked", callback, data);
+	gtk_box_pack_start(GTK_BOX(hbox), widget, FALSE, TRUE, 0);
+	return hbox;
 }
 
 static GtkWidget * _init_label(GtkSizeGroup * group, char const * label,
@@ -146,7 +220,11 @@ static GtkWidget * _init_label(GtkSizeGroup * group, char const * label,
 static void _cvs_destroy(BrowserPlugin * plugin)
 {
 	CVS * cvs = plugin->priv;
+	size_t i;
 
+	for(i = 0; i < cvs->tasks_cnt; i++)
+		_cvs_task_delete(cvs->tasks[i]);
+	free(cvs->tasks);
 	if(cvs->source != 0)
 		g_source_remove(cvs->source);
 	object_delete(cvs);
@@ -311,4 +389,239 @@ static void _refresh_status(CVS * cvs, char const * status)
 	if(status == NULL)
 		status = "";
 	gtk_label_set_text(GTK_LABEL(cvs->status), status);
+}
+
+
+/* cvs_add_task */
+static int _cvs_add_task(BrowserPlugin * plugin, char const * directory,
+		char * argv[])
+{
+	BrowserPluginHelper * helper = plugin->helper;
+	CVS * cvs = plugin->priv;
+	CVSTask ** p;
+	CVSTask * task;
+	gboolean res;
+	GError * error = NULL;
+	PangoFontDescription * font;
+	GtkWidget * widget;
+
+	if((p = realloc(cvs->tasks, sizeof(*p) * (cvs->tasks_cnt + 1))) == NULL)
+		return -helper->error(helper->browser, "malloc", 1);
+	cvs->tasks = p;
+	if((task = object_new(sizeof(*task))) == NULL)
+		return -helper->error(helper->browser, error_get(), 1);
+	task->plugin = plugin;
+	res = g_spawn_async_with_pipes(directory, argv, NULL,
+			G_SPAWN_SEARCH_PATH, NULL, NULL, &task->pid, NULL,
+			&task->o_fd, &task->e_fd, &error);
+	if(res != TRUE)
+	{
+		helper->error(helper->browser, error->message, 1);
+		g_error_free(error);
+		object_delete(task);
+		return -1;
+	}
+	cvs->tasks[cvs->tasks_cnt++] = task;
+	/* widgets */
+	font = pango_font_description_new();
+	pango_font_description_set_family(font, "monospace");
+	task->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size(GTK_WINDOW(task->window), 600, 400);
+#if GTK_CHECK_VERSION(2, 6, 0)
+	gtk_window_set_icon_name(GTK_WINDOW(task->window), plugin->icon);
+#endif
+	gtk_window_set_title(GTK_WINDOW(task->window), cvs->filename);
+	g_signal_connect_swapped(task->window, "delete-event", G_CALLBACK(
+				_cvs_task_on_closex), task);
+	widget = gtk_scrolled_window_new(NULL, NULL);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(widget),
+			GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	task->view = gtk_text_view_new();
+	gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(task->view), FALSE);
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(task->view), FALSE);
+	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(task->view),
+			GTK_WRAP_WORD_CHAR);
+	gtk_widget_modify_font(task->view, font);
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(widget),
+			task->view);
+	gtk_container_add(GTK_CONTAINER(task->window), widget);
+	gtk_widget_show_all(task->window);
+	pango_font_description_free(font);
+	/* events */
+	task->o_channel = g_io_channel_unix_new(task->o_fd);
+	if((g_io_channel_set_encoding(task->o_channel, NULL, &error))
+			!= G_IO_STATUS_NORMAL)
+	{
+		helper->error(helper->browser, error->message, 1);
+		g_error_free(error);
+	}
+	task->o_source = g_io_add_watch(task->o_channel, G_IO_IN,
+			_cvs_task_on_io_can_read, task);
+	task->e_channel = g_io_channel_unix_new(task->e_fd);
+	if((g_io_channel_set_encoding(task->e_channel, NULL, &error))
+			!= G_IO_STATUS_NORMAL)
+	{
+		helper->error(helper->browser, error->message, 1);
+		g_error_free(error);
+	}
+	task->e_source = g_io_add_watch(task->e_channel, G_IO_IN,
+			_cvs_task_on_io_can_read, task);
+	return 0;
+}
+
+
+/* tasks */
+/* cvs_task_delete */
+static void _cvs_task_delete(CVSTask * task)
+{
+	_cvs_task_close(task);
+	gtk_widget_destroy(task->window);
+	object_delete(task);
+}
+
+
+/* cvs_task_close */
+static void _cvs_task_close(CVSTask * task)
+{
+	_cvs_task_close_channel(task, task->o_channel);
+	_cvs_task_close_channel(task, task->e_channel);
+}
+
+
+/* cvs_task_close */
+static void _cvs_task_close_channel(CVSTask * task, GIOChannel * channel)
+{
+	if(channel != NULL && channel == task->o_channel)
+	{
+		if(task->o_source != 0)
+			g_source_remove(task->o_source);
+		task->o_source = 0;
+		g_io_channel_shutdown(task->o_channel, FALSE, NULL);
+		g_io_channel_unref(task->o_channel);
+		task->o_channel = NULL;
+	}
+	if(channel != NULL && task->e_channel != NULL)
+	{
+		if(task->e_source != 0)
+			g_source_remove(task->e_source);
+		task->e_source = 0;
+		g_io_channel_shutdown(task->e_channel, FALSE, NULL);
+		g_io_channel_unref(task->e_channel);
+		task->e_channel = NULL;
+	}
+}
+
+
+/* callbacks */
+/* cvs_on_commit */
+static void _cvs_on_commit(gpointer data)
+{
+	BrowserPlugin * plugin = data;
+	CVS * cvs = plugin->priv;
+	gchar * basename;
+	gchar * dirname;
+	char * argv[] = { "cvs", "commit", NULL, NULL };
+
+	if(cvs->filename == NULL)
+		return;
+	basename = g_path_get_basename(cvs->filename);
+	dirname = g_path_get_dirname(cvs->filename);
+	argv[2] = basename;
+	_cvs_add_task(plugin, dirname, argv);
+	g_free(dirname);
+	g_free(basename);
+}
+
+
+/* cvs_on_diff */
+static void _cvs_on_diff(gpointer data)
+{
+	BrowserPlugin * plugin = data;
+	CVS * cvs = plugin->priv;
+	gchar * basename;
+	gchar * dirname;
+	char * argv[] = { "cvs", "diff", NULL, NULL };
+
+	if(cvs->filename == NULL)
+		return;
+	basename = g_path_get_basename(cvs->filename);
+	dirname = g_path_get_dirname(cvs->filename);
+	argv[2] = basename;
+	_cvs_add_task(plugin, dirname, argv);
+	g_free(dirname);
+	g_free(basename);
+}
+
+
+/* cvs_on_update */
+static void _cvs_on_update(gpointer data)
+{
+	BrowserPlugin * plugin = data;
+	CVS * cvs = plugin->priv;
+	gchar * basename;
+	gchar * dirname;
+	char * argv[] = { "cvs", "update", NULL, NULL };
+
+	if(cvs->filename == NULL)
+		return;
+	basename = g_path_get_basename(cvs->filename);
+	dirname = g_path_get_dirname(cvs->filename);
+	argv[2] = basename;
+	_cvs_add_task(plugin, dirname, argv);
+	g_free(dirname);
+	g_free(basename);
+}
+
+
+/* cvs_task_on_closex */
+static gboolean _cvs_task_on_closex(gpointer data)
+{
+	CVSTask * task = data;
+
+	gtk_widget_hide(task->window);
+	_cvs_task_close(task);
+	/* FIXME really implement */
+	return TRUE;
+}
+
+
+/* cvs_task_on_io_can_read */
+static gboolean _cvs_task_on_io_can_read(GIOChannel * channel,
+		GIOCondition condition, gpointer data)
+{
+	CVSTask * task = data;
+	BrowserPlugin * plugin = task->plugin;
+	BrowserPluginHelper * helper = plugin->helper;
+	char buf[256];
+	gsize cnt = 0;
+	GError * error = NULL;
+	GIOStatus status;
+	GtkTextBuffer * tbuf;
+	GtkTextIter iter;
+
+	if(condition != G_IO_IN)
+		return FALSE;
+	if(channel != task->o_channel && channel != task->e_channel)
+		return FALSE;
+	status = g_io_channel_read_chars(channel, buf, sizeof(buf), &cnt,
+			&error);
+	if(cnt > 0)
+	{
+		tbuf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(task->view));
+		gtk_text_buffer_get_end_iter(tbuf, &iter);
+		gtk_text_buffer_insert(tbuf, &iter, buf, cnt);
+	}
+	switch(status)
+	{
+		case G_IO_STATUS_NORMAL:
+			break;
+		case G_IO_STATUS_ERROR:
+			helper->error(helper->browser, error->message, 1);
+			g_error_free(error);
+		case G_IO_STATUS_EOF:
+		default: /* should not happen... */
+			_cvs_task_close_channel(task, channel);
+			return FALSE;
+	}
+	return TRUE;
 }
