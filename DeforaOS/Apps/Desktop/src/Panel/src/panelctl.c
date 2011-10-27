cf94849a50eb0a6975479da33983b61bf2b6cdae
khorben <>
2011-10-27 14:32:10 +0000

    Renamed "panel-settings" to "panelctl" (with the "-S" flag)

--- DeforaOS/Apps/Desktop/src/Panel/src/panelctl.c
+++ DeforaOS/Apps/Desktop/src/Panel/src/panelctl.c
@@ -22,28 +22,19 @@
 #include "common.h"
 
 
-/* usage */
-static int _usage(void)
-{
-	fputs("Usage: panel-settings\n", stderr);
-	return 1;
-}
+/* panelctl */
+/* private */
+/* prototypes */
+static int _panelctl(PanelMessageShow show);
+static int _usage(void);
 
 
-/* main */
-int main(int argc, char * argv[])
+/* functions */
+static int _panelctl(PanelMessageShow show)
 {
-	int o;
 	GdkEvent event;
 	GdkEventClient * client = &event.client;
 
-	gtk_init(&argc, &argv);
-	while((o = getopt(argc, argv, "")) != -1)
-		switch(o)
-		{
-			default:
-				return _usage();
-		}
 	memset(&event, 0, sizeof(event));
 	client->type = GDK_CLIENT_EVENT;
 	client->window = NULL;
@@ -51,8 +42,41 @@ int main(int argc, char * argv[])
 	client->message_type = gdk_atom_intern(PANEL_CLIENT_MESSAGE, FALSE);
 	client->data_format = 8;
 	client->data.b[0] = PANEL_MESSAGE_SHOW;
-	client->data.b[1] = PANEL_MESSAGE_SHOW_SETTINGS;
+	client->data.b[1] = show;
 	client->data.b[2] = TRUE;
 	gdk_event_send_clientmessage_toall(&event);
 	return 0;
+}
+
+
+/* usage */
+static int _usage(void)
+{
+	fputs("Usage: panelctl -S\n"
+"  -S	Display or change settings\n", stderr);
+	return 1;
+}
+
+
+/* public */
+/* functions */
+/* main */
+int main(int argc, char * argv[])
+{
+	int o;
+	int show = -1;
+
+	gtk_init(&argc, &argv);
+	while((o = getopt(argc, argv, "S")) != -1)
+		switch(o)
+		{
+			case 'S':
+				show = PANEL_MESSAGE_SHOW_SETTINGS;
+				break;
+			default:
+				return _usage();
+		}
+	if(argc != optind || show < 0)
+		return _usage();
+	return (_panelctl(show) == 0) ? 0 : 2;
 }
