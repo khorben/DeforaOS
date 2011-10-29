fedaf93d7806efa348b7559c8ea4fb1f61030a53
khorben <>
2011-10-29 01:55:48 +0000

    Added a feature request

--- DeforaOS/System/src/libSystem/src/config.c
+++ DeforaOS/System/src/libSystem/src/config.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2010 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2011 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libSystem */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,6 +12,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/* TODO:
+ * - offer a way to load preferences (optionally from the system first) */
 
 
 
