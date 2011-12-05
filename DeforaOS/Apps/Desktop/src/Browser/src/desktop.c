f5892ba1e5bd9d441bdd936ddd4223f6c2f808b3
khorben <>
2011-12-05 09:47:22 +0000

    More things to do

--- DeforaOS/Apps/Desktop/src/Browser/src/desktop.c
+++ DeforaOS/Apps/Desktop/src/Browser/src/desktop.c
@@ -12,8 +12,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-/* FIXME
- * - let the user define the desktop folder
+/* TODO:
+ * - let the user define the desktop folder (possibly default to FDO's)
+ * - set the font for the icons instantly
  * - track multiple selection on delete/properties */
 
 
