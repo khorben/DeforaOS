a241e645f7338b981c19fe839dc3b1fdd5644975
khorben <>
2012-01-07 03:40:11 +0000

    About to release Browser 0.4.4

--- DeforaOS/Apps/Desktop/src/Browser/config.h
+++ DeforaOS/Apps/Desktop/src/Browser/config.h
@@ -1,5 +1,5 @@
 #define PACKAGE "Browser"
-#define VERSION "0.4.3"
+#define VERSION "0.4.4"
 
 #ifndef PREFIX
 # define PREFIX "/usr/local"
--- DeforaOS/Apps/Desktop/src/Browser/config.sh
+++ DeforaOS/Apps/Desktop/src/Browser/config.sh
@@ -1,5 +1,5 @@
 PACKAGE="Browser"
-VERSION="0.4.3"
+VERSION="0.4.4"
 
 PREFIX="/usr/local"
 LIBDIR="${PREFIX}/lib"
