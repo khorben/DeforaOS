a65069a03f6cbafec82a2e63143f343999adfcb1
khorben <>
2012-01-09 17:54:19 +0000

    Initial import

---
+++ DeforaOS/Apps/Web/DaPortal/src/templates/desktop.php
@@ -0,0 +1,46 @@
+<?php //$Id$
+//Copyright (c) 2012 Pierre Pronchery <khorben@defora.org>
+//This file is part of DeforaOS Web DaPortal
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, version 3 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+//DesktopTemplate
+require_once('./system/template.php');
+class DesktopTemplate extends Template
+{
+	//protected
+	//methods
+	//DesktopTemplate::match
+	protected function match(&$engine)
+	{
+		return 100;
+	}
+
+
+	//DesktopTemplate::attach
+	protected function attach(&$engine)
+	{
+	}
+
+
+	//DesktopTemplate::render
+	public function render(&$engine, $page)
+	{
+		//FIXME implement
+		return $page;
+	}
+};
+
+?>
