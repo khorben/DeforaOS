60438b1b5f884da7c87ae6ea21881e38c758b30e
khorben <>
2012-02-05 00:03:07 +0000

    Code cleanup

--- DeforaOS/Apps/Web/DaPortal/system/mail.php
+++ DeforaOS/Apps/Web/DaPortal/system/mail.php
@@ -1,5 +1,5 @@
-<?php  //$Id$
-//Copyright (c) 2007 Pierre Pronchery <khorben@defora.org>
+<?php //$Id$
+//Copyright (c) 2007-2012 Pierre Pronchery <khorben@defora.org>
 //This file is part of DaPortal
 //
 //DaPortal is free software; you can redistribute it and/or modify
