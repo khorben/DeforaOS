21b6be2e49d241495880f8b1343bd50cb713566c
khorben <>
2012-02-06 01:47:13 +0000

    Updated copyright information

--- DeforaOS/Apps/Web/DaPortal/src/database/sqlite2.php
+++ DeforaOS/Apps/Web/DaPortal/src/database/sqlite2.php
@@ -1,5 +1,5 @@
 <?php //$Id$
-//Copyright (c) 2011 Pierre Pronchery <khorben@defora.org>
+//Copyright (c) 2011-2012 Pierre Pronchery <khorben@defora.org>
 //This file is part of DeforaOS Web DaPortal
 //
 //This program is free software: you can redistribute it and/or modify
