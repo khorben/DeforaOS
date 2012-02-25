6598286781db5d3ec3248a52794a8db8417f01d3
khorben <>
2012-02-25 22:59:37 +0000

    Removed unused variable

--- DeforaOS/Apps/Desktop/src/Surfer/src/callbacks.c
+++ DeforaOS/Apps/Desktop/src/Surfer/src/callbacks.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2011 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2006-2012 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Desktop Surfer */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -466,7 +466,6 @@ static gboolean _switch_page_idle(gpointer data)
 	Surfer * surfer = data;
 	gint n;
 	GtkWidget * ghtml;
-	gdouble zoom;
 
 	if((n = gtk_notebook_get_current_page(GTK_NOTEBOOK(surfer->notebook)))
 			< 0)
