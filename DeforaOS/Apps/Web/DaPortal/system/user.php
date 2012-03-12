acc41d6f6dc008347904d369bab228eef745b760
khorben <>
2012-03-12 17:10:13 +0000

    Using the SQL boolean helper

--- DeforaOS/Apps/Web/DaPortal/system/user.php
+++ DeforaOS/Apps/Web/DaPortal/system/user.php
@@ -21,14 +21,16 @@
 //	0	is not admin
 function _user_admin($id)
 {
+	global $engine;
 	static $cache = array();
+	$db = $engine->getDatabase();
 
 	if(!is_numeric($id))
 		return 0;
 	if(array_key_exists($id, $cache))
 		return $cache[$id];
-	$cache[$id] = _sql_single('SELECT admin FROM daportal_user'
-			." WHERE user_id='$id' AND enabled='1'") == SQL_TRUE;
+	$cache[$id] = $db->isTrue(_sql_single('SELECT admin FROM daportal_user'
+			." WHERE user_id='$id' AND enabled='1'"));
 	return $cache[$id];
 }
 
