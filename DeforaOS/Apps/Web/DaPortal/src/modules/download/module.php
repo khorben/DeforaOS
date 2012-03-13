3f6f7860075b6e8d6955594332f40030059b6ef5
khorben <>
2012-03-13 01:26:44 +0000

    Implementing file uploads

--- DeforaOS/Apps/Web/DaPortal/src/modules/download/module.php
+++ DeforaOS/Apps/Web/DaPortal/src/modules/download/module.php
@@ -16,6 +16,7 @@
 
 
 
+require_once('./system/content.php');
 require_once('./modules/content/module.php');
 
 
@@ -24,6 +25,30 @@ class DownloadModule extends ContentModule
 {
 	//public
 	//methods
+	//forms
+	//DownloadModule::form_file_insert
+	protected function form_file_insert($engine, $request)
+	{
+		$parent = $request->getParameter('parent');
+
+		if(!is_numeric($parent))
+			$parent = FALSE;
+		$r = new Request($engine, $this->name, 'file_insert');
+		if($parent !== FALSE)
+			//FIXME also tell where the upload is for
+			$r->setParameter('parent', $parent);
+		$form = new PageElement('form', array('request' => $r));
+		$form->append('filechooser', array('name' => 'files[]'));
+		$form->append('filechooser', array('name' => 'files[]'));
+		$r = new Request($engine, $this->name, FALSE, $parent);
+		$form->append('button', array('text' => _('Cancel'),
+				'stock' => 'cancel', 'request' => $r));
+		$form->append('button', array('type' => 'submit',
+				'stock' => 'upload', 'text' => _('Upload')));
+		return $form;
+	}
+
+
 	//essential
 	//DownloadModule::DownloadModule
 	public function __construct($id, $name)
@@ -47,6 +72,8 @@ class DownloadModule extends ContentModule
 		{
 			case 'download':
 				return $this->download($engine, $request);
+			case 'file_insert':
+				return $this->fileInsert($engine, $request);
 		}
 		return parent::call($engine, $request);
 	}
@@ -72,10 +99,102 @@ class DownloadModule extends ContentModule
 
 
 	//methods
+	//DownloadModule::download
 	protected function download($engine, $request)
 	{
 		//FIXME really implement
 		return $this->preview($engine, $request);
+	}
+
+
+	//DownloadModule::fileInsert
+	protected function fileInsert($engine, $request)
+	{
+		$title = _('File upload');
+		$credentials = $engine->getCredentials();
+
+		//check permissions
+		if(!$credentials->isAdmin())
+			return new PageElement('dialog', array(
+				'type' => 'error',
+				'text' => _('Permission denied')));
+		$page = new Page(array('title' => $title));
+		$page->append('title', array('stock' => 'upload',
+			'text' => $title));
+		//process the upload
+		$error = $this->_fileInsert($engine, $request);
+		//upload successful
+		if($error === FALSE)
+		{
+			//FIXME really link to the parent directory
+			$r = new Request($engine, $this->name);
+			$page->setProperty('location', $engine->getUrl($r));
+			$page->setProperty('refresh', 30);
+			$box = $page->append('vbox');
+			$text = _('Upload in progress, please wait...');
+			$box->append('label', array('text' => $text));
+			$box = $box->append('hbox');
+			$text = _('If you are not redirected within 30 seconds, please ');
+			$box->append('label', array('text' => $text));
+			$box->append('link', array('text' => _('click here'),
+					'request' => $r));
+			$box->append('label', array('text' => '.'));
+			return $page;
+		}
+		else if(is_string($error))
+			$page->append('dialog', array('type' => 'error',
+					'text' => $error));
+		$form = $this->form_file_insert($engine, $request);
+		$page->appendElement($form);
+		return $page;
+	}
+
+	private function _fileInsert($engine, $request)
+	{
+		$db = $engine->getDatabase();
+
+		//FIXME really lookup
+		$root = '/tmp';
+		if(!isset($_FILES['files']))
+			return TRUE;
+		if($engine->isIdempotent($request) !== FALSE)
+			return _('The request expired or is invalid');
+		$parent = $request->getParameter('parent');
+		if($parent !== FALSE && !is_numeric($parent))
+			return _('Invalid argument');
+		var_dump($_FILES);
+		foreach($_FILES['files']['error'] as $k => $v)
+			if($v != UPLOAD_ERR_OK)
+				return _('An error occurred');
+		if($db->transactionBegin($engine) === FALSE)
+			return _('Internal server error');
+		foreach($_FILES['files']['error'] as $k => $v)
+		{
+			$name = $_FILES['files']['name'][$k];
+			//FIXME check for filename unicity
+			$content = Content::insert($engine, $this->id, $name,
+					FALSE, TRUE, FALSE);
+			if($content === FALSE)
+			{
+				$db->transactionRollback();
+				return _('Internal server error');
+			}
+			$id = $content->getId();
+			$tmp = $_FILES['files']['tmp_name'][$k];
+			if(move_uploaded_file($tmp, $root.'/'.$id) !== TRUE)
+			{
+				$db->transactionRollback();
+				return _('Internal server error');
+			}
+		}
+		//FIXME really implement
+		{
+			$db->transactionRollback();
+			return _('Not fully implemented');
+		}
+		if($db->transactionCommit($engine) === FALSE)
+			return _('Internal server error');
+		return FALSE;
 	}
 }
 
