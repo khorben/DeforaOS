2ea35f778e8be8140ee8b86f42ca1636e24dfb46
khorben <>
2012-05-20 10:50:54 +0000

    Removed bogus licensing statement

--- DeforaOS/Apps/Desktop/src/Browser/po/gettext.sh
+++ DeforaOS/Apps/Desktop/src/Browser/po/gettext.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #$Id$
 #Copyright (c) 2011 Pierre Pronchery <khorben@defora.org>
-#All rights reserved.
 #
 #Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
