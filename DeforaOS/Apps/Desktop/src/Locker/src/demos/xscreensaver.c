8a3d5fb5cad582f4ab123c8f074ad5ef8f0519ce
khorben <>
2012-07-14 20:52:20 +0000

    Stopping and starting the child xscreensaver process(es) as required

--- DeforaOS/Apps/Desktop/src/Locker/src/demos/xscreensaver.c
+++ DeforaOS/Apps/Desktop/src/Locker/src/demos/xscreensaver.c
@@ -12,14 +12,13 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-/* TODO:
- * - send SIGSTOP/SIGSTART to the child process when its window is removed? */
 
 
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 #include <errno.h>
 #include <gdk/gdkx.h>
 #include <System.h>
@@ -34,9 +33,19 @@
 /* XScreensaver */
 /* private */
 /* types */
+typedef struct _XScreensaverWindow
+{
+	GtkWidget * window;
+	GPid pid;
+} XScreensaverWindow;
+
 typedef struct _LockerDemo
 {
 	LockerDemoHelper * helper;
+
+	/* windows */
+	XScreensaverWindow * windows;
+	size_t windows_cnt;
 } XScreensaver;
 
 
@@ -79,6 +88,8 @@ static XScreensaver * _xscreensaver_init(LockerDemoHelper * helper)
 	if((xscreensaver = object_new(sizeof(*xscreensaver))) == NULL)
 		return NULL;
 	xscreensaver->helper = helper;
+	xscreensaver->windows = NULL;
+	xscreensaver->windows_cnt = 0;
 	return xscreensaver;
 }
 
@@ -86,15 +97,19 @@ static XScreensaver * _xscreensaver_init(LockerDemoHelper * helper)
 /* xscreensaver_destroy */
 static void _xscreensaver_destroy(XScreensaver * xscreensaver)
 {
+	free(xscreensaver->windows);
 	object_delete(xscreensaver);
 }
 
 
 /* xscreensaver_add */
+static XScreensaverWindow * _add_allocate(XScreensaver * xscreensaver);
+
 static int _xscreensaver_add(XScreensaver * xscreensaver, GtkWidget * window)
 {
 	int ret = 0;
 	LockerDemoHelper * helper = xscreensaver->helper;
+	XScreensaverWindow * w;
 	unsigned int id = GDK_WINDOW_XWINDOW(window->window);
 	GError * error = NULL;
 	char * argv[] = { NULL, "-window-id", NULL, NULL };
@@ -104,6 +119,8 @@ static int _xscreensaver_add(XScreensaver * xscreensaver, GtkWidget * window)
 #ifdef DEBUG
 	fprintf(stderr, "DEBUG: %s() %u\n", __func__, id);
 #endif
+	if((w = _add_allocate(xscreensaver)) == NULL)
+		return -1; /* XXX report error */
 	if((p = helper->config_get(helper->locker, "xscreensaver",
 					"xscreensaver")) != NULL)
 		argv[0] = strdup(p);
@@ -113,12 +130,34 @@ static int _xscreensaver_add(XScreensaver * xscreensaver, GtkWidget * window)
 	argv[2] = buf;
 	if(argv[0] == NULL)
 		return -helper->error(NULL, strerror(errno), 1);
-	if(g_spawn_async(NULL, argv, NULL, 0, NULL, NULL, NULL, &error) != TRUE)
+	if(g_spawn_async(NULL, argv, NULL, 0, NULL, NULL, &w->pid, &error)
+			!= TRUE)
 	{
 		ret = helper->error(NULL, error->message, 1);
 		g_error_free(error);
 	}
+	w->window = window;
 	return ret;
+}
+
+static XScreensaverWindow * _add_allocate(XScreensaver * xscreensaver)
+{
+	XScreensaverWindow * w;
+	size_t i;
+
+	/* look for a free window */
+	for(i = 0; i < xscreensaver->windows_cnt; i++)
+		if(xscreensaver->windows[i].window == NULL)
+			return &xscreensaver->windows[i];
+	/* allocate a window */
+	i = xscreensaver->windows_cnt + 1;
+	if((w = realloc(xscreensaver->windows, sizeof(*w) * i)) == NULL)
+		return NULL;
+	xscreensaver->windows = w;
+	w = &xscreensaver->windows[xscreensaver->windows_cnt++];
+	w->window = NULL;
+	w->pid = -1;
+	return w;
 }
 
 
@@ -133,12 +172,20 @@ static void _xscreensaver_remove(XScreensaver * xscreensaver,
 /* xscreensaver_start */
 static void _xscreensaver_start(XScreensaver * xscreensaver)
 {
-	/* FIXME implement */
+	size_t i;
+
+	for(i = 0; i < xscreensaver->windows_cnt; i++)
+		if(xscreensaver->windows[i].pid > 0)
+			kill(xscreensaver->windows[i].pid, SIGCONT);
 }
 
 
 /* xscreensaver_stop */
 static void _xscreensaver_stop(XScreensaver * xscreensaver)
 {
-	/* FIXME implement */
+	size_t i;
+
+	for(i = 0; i < xscreensaver->windows_cnt; i++)
+		if(xscreensaver->windows[i].pid > 0)
+			kill(xscreensaver->windows[i].pid, SIGSTOP);
 }
