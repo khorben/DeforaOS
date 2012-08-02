3c536350093cd5cae5b211edb318195735e455c7
khorben <>
2012-08-02 12:55:58 +0000

    Fixed switching to the authentication widgets in a generic manner

--- DeforaOS/Apps/Desktop/src/Locker/src/demos/gtk-demo.c
+++ DeforaOS/Apps/Desktop/src/Locker/src/demos/gtk-demo.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 /* TODO:
  * - re-factor and re-indent adequately
- * - no longer obfuscate the authentication widgets
  * - improve performance */
 
 
