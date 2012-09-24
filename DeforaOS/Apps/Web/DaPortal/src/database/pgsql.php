684785ba37c1cd0c36611d3df9c498d9829c0e09
khorben <>
2012-09-24 18:40:30 +0000

    Working on timezone issues

--- DeforaOS/Apps/Web/DaPortal/src/database/pgsql.php
+++ DeforaOS/Apps/Web/DaPortal/src/database/pgsql.php
@@ -80,6 +80,24 @@ class PgsqlDatabase extends Database
 	}
 
 
+	//PgsqlDatabase::formatDate
+	public function formatDate($engine, $date, $outformat = FALSE,
+			$informat = FALSE)
+	{
+		if($informat === FALSE)
+			$informat = '%Y-%m-%d %H:%M:%S';
+		if(($tm = strptime($date, $informat)) === FALSE)
+			return $date; //XXX better suggestions welcome
+		//FIXME this assumes the database server shares our timezone
+		$timestamp = mktime($tm['tm_hour'], $tm['tm_min'],
+			$tm['tm_sec'], $tm['tm_mon'] + 1, $tm['tm_mday'],
+			$tm['tm_year'] + 1900);
+		if($outformat === FALSE)
+			$outformat = '%d/%m/%Y %H:%M:%S';
+		return strftime($outformat, $timestamp);
+	}
+
+
 	//PgsqlDatabase::offset
 	public function offset($limit, $offset = FALSE)
 	{
